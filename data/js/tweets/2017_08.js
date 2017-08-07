fe984cb993b2e94ba4409357bee7656f91f465af
bhwilkoff <benwilkoff@gmail.com>
2017-08-07 16:22:02 -0600

    Added 1 tweets

--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,160 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Mon Aug 07 21:50:40 +0000 2017",
+		"id": 894677194340933600,
+		"id_str": "894677194340933632",
+		"text": "@dolanreads Absolutely! I’m gonna mention @EdTechARose, as she supports basing in @aurorak12. Let’s talk!",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dolanreads",
+					"name": "Teresa Dolan",
+					"id": 714843979,
+					"id_str": "714843979",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "EdTechARose",
+					"name": "Amanda Rose Fuller",
+					"id": 3311854374,
+					"id_str": "3311854374",
+					"indices": [
+						42,
+						54
+					]
+				},
+				{
+					"screen_name": "aurorak12",
+					"name": "Aurora PublicSchools",
+					"id": 31184923,
+					"id_str": "31184923",
+					"indices": [
+						82,
+						92
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 894584709824557000,
+		"in_reply_to_status_id_str": "894584709824557061",
+		"in_reply_to_user_id": 714843979,
+		"in_reply_to_user_id_str": "714843979",
+		"in_reply_to_screen_name": "dolanreads",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Director of Personalized Learning for @aurorak12. Passionate about equity, agency, and creating one new thing every day. In short, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6316,
+			"friends_count": 2478,
+			"listed_count": 393,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 655,
+			"utc_offset": -21600,
+			"time_zone": "Mountain Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17379,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/491548197668405248/VVuZKH3d_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/491548197668405248/VVuZKH3d_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1398689493",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "5369fb7723bcfaca",
+			"url": "https://api.twitter.com/1.1/geo/id/5369fb7723bcfaca.json",
+			"place_type": "neighborhood",
+			"name": "Memorial",
+			"full_name": "Memorial, Houston",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-95.644794,
+							29.74583
+						],
+						[
+							-95.525801,
+							29.74583
+						],
+						[
+							-95.525801,
+							29.79059
+						],
+						[
+							-95.644794,
+							29.79059
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Aug 07 13:34:26 +0000 2017",
 		"id": 894552316136890400,
 		"id_str": "894552316136890368",

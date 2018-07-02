267b3cee5bab7245e067336b8aa43708dd0b1cf6
bhwilkoff <benwilkoff@gmail.com>
2018-07-02 15:22:05 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17828,
-	"created_at": "Wed Jun 27 21:22:04 +0000 2018",
+	"tweets": 17829,
+	"created_at": "Mon Jul 02 21:22:04 +0000 2018",
 	"lang": "en"
 }
---
+++ data/js/tweets/2018_07.js
@@ -0,0 +1,258 @@
+Grailbird.data.tweets_2018_07 = 
+[
+	{
+		"created_at": "Mon Jul 02 20:34:25 +0000 2018",
+		"id": 1013883616944746500,
+		"id_str": "1013883616944746496",
+		"text": "It is my first day today! So far so good. https://t.co/bn34FfOuiH",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bn34FfOuiH",
+					"expanded_url": "https://twitter.com/CUAnschutz/status/1013877067140739072",
+					"display_url": "twitter.com/CUAnschutz/sta…",
+					"indices": [
+						42,
+						65
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6424,
+			"friends_count": 2926,
+			"listed_count": 385,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 812,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17797,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/491548197668405248/VVuZKH3d_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/491548197668405248/VVuZKH3d_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1398689493",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "b49b3053b5c25bf5",
+			"url": "https://api.twitter.com/1.1/geo/id/b49b3053b5c25bf5.json",
+			"place_type": "city",
+			"name": "Denver",
+			"full_name": "Denver, CO",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-105.109815,
+							39.614151
+						],
+						[
+							-104.734372,
+							39.614151
+						],
+						[
+							-104.734372,
+							39.812975
+						],
+						[
+							-105.109815,
+							39.812975
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1013877067140739100,
+		"quoted_status_id_str": "1013877067140739072",
+		"quoted_status": {
+			"created_at": "Mon Jul 02 20:08:24 +0000 2018",
+			"id": 1013877067140739100,
+			"id_str": "1013877067140739072",
+			"text": "@wfryer @bhwilkoff Welcome, Ben! We are so excited to have you join us and can't wait to see what you do with us! #CUAnschutz",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "CUAnschutz",
+						"indices": [
+							114,
+							125
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "wfryer",
+						"name": "Wesley Fryer 🌎🎙🚀",
+						"id": 7095342,
+						"id_str": "7095342",
+						"indices": [
+							0,
+							7
+						]
+					},
+					{
+						"screen_name": "bhwilkoff",
+						"name": "Ben Wilkoff",
+						"id": 1710871,
+						"id_str": "1710871",
+						"indices": [
+							8,
+							18
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"https://sproutsocial.com\" rel=\"nofollow\">Sprout Social</a>",
+			"in_reply_to_status_id": 1013116445671264300,
+			"in_reply_to_status_id_str": "1013116445671264256",
+			"in_reply_to_user_id": 7095342,
+			"in_reply_to_user_id_str": "7095342",
+			"in_reply_to_screen_name": "wfryer",
+			"user": {
+				"id": 761688578,
+				"id_str": "761688578",
+				"name": "CU Anschutz",
+				"screen_name": "CUAnschutz",
+				"location": "Aurora, CO",
+				"description": "Official Twitter account of the University of Colorado Anschutz Medical Campus, a 21st century healthcare city. #CUAnschutz",
+				"url": "https://t.co/VwTuxHbwfN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/VwTuxHbwfN",
+								"expanded_url": "http://www.ucdenver.edu/anschutz",
+								"display_url": "ucdenver.edu/anschutz",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5792,
+				"friends_count": 1919,
+				"listed_count": 110,
+				"created_at": "Thu Aug 16 14:47:37 +0000 2012",
+				"favourites_count": 4616,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5971,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/887786136466214912/qIFFFsi5_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/887786136466214912/qIFFFsi5_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/761688578/1530308833",
+				"profile_link_color": "B89E65",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]

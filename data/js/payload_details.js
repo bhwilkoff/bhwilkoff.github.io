c14d7b6ea1068c56d7ff91660bea002fa3f464a8
bhwilkoff <benwilkoff@gmail.com>
2018-11-02 14:23:31 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17928,
-	"created_at": "Tue Oct 30 21:22:01 +0000 2018",
+	"tweets": 17929,
+	"created_at": "Fri Nov 02 20:23:30 +0000 2018",
 	"lang": "en"
 }
---
+++ data/js/tweets/2018_11.js
@@ -0,0 +1,233 @@
+Grailbird.data.tweets_2018_11 = 
+[
+	{
+		"created_at": "Fri Nov 02 19:22:33 +0000 2018",
+		"id": 1058439234464309200,
+		"id_str": "1058439234464309248",
+		"full_text": "Just made a quick shortcut to allow for mobile annotation on iOS (iPad and iPhone) using @hypothes_is. It accepts URLs for quickly opening annotations in your mobile browser. Super fun! https://t.co/LnY2Q109nY /w @remikalir @dr_jdean and many awesome others at #annotatED18 #EDU18 https://t.co/MGMiRioETr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "annotatED18",
+					"indices": [
+						261,
+						273
+					]
+				},
+				{
+					"text": "EDU18",
+					"indices": [
+						274,
+						280
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hypothes_is",
+					"name": "Hypothesis",
+					"id": 325100970,
+					"id_str": "325100970",
+					"indices": [
+						89,
+						101
+					]
+				},
+				{
+					"screen_name": "remikalir",
+					"name": "Remi Kalir",
+					"id": 2557762548,
+					"id_str": "2557762548",
+					"indices": [
+						213,
+						223
+					]
+				},
+				{
+					"screen_name": "dr_jdean",
+					"name": "(((Dr. Dean)))",
+					"id": 739521654,
+					"id_str": "739521654",
+					"indices": [
+						224,
+						233
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/LnY2Q109nY",
+					"expanded_url": "https://www.icloud.com/shortcuts/ec0e894030474a37875320635ccc9e6f",
+					"display_url": "icloud.com/shortcuts/ec0e…",
+					"indices": [
+						186,
+						209
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1058439232950218800,
+					"id_str": "1058439232950218752",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/DrBU3-BVsAA2d0N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrBU3-BVsAA2d0N.jpg",
+					"url": "https://t.co/MGMiRioETr",
+					"display_url": "pic.twitter.com/MGMiRioETr",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1058439234464309248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1058439232950218800,
+					"id_str": "1058439232950218752",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/DrBU3-BVsAA2d0N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrBU3-BVsAA2d0N.jpg",
+					"url": "https://t.co/MGMiRioETr",
+					"display_url": "pic.twitter.com/MGMiRioETr",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1058439234464309248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://pragmaticcode.com/linky/\" rel=\"nofollow\">Linky for iOS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6310,
+			"friends_count": 201,
+			"listed_count": 380,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 839,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17894,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1398689493",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just made a quick shortcut to allow for mobile annotation on iOS (iPad and iPhone) using @hypothes_is. It accepts URLs for quickly opening annotations in your mobile browser. Super fun! https://t.co/LnY2Q109nY /w @remikalir @dr_jdean and many awesome others at #annotatED18 #EDU18 https://t.co/MGMiRioETr"
+	}
+]

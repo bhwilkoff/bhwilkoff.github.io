826f1f53186b53e37e1ea909a16bf04d01753bed
bhwilkoff <benwilkoff@gmail.com>
2019-01-15 09:22:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17981,
-	"created_at": "Sat Jan 12 17:22:00 +0000 2019",
+	"tweets": 17982,
+	"created_at": "Tue Jan 15 16:22:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 15 16:06:33 +0000 2019",
+		"id": 1085206615198916600,
+		"id_str": "1085206615198916608",
+		"full_text": "This is a book worth reading: A Heart in a Body in the World https://t.co/xEH716VDHD #YAfiction https://t.co/7RUuMFdrol",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YAfiction",
+					"indices": [
+						85,
+						95
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xEH716VDHD",
+					"expanded_url": "https://www.goodreads.com/work/best_book/59926250-a-heart-in-a-body-in-the-world",
+					"display_url": "goodreads.com/work/best_book…",
+					"indices": [
+						61,
+						84
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1085206613005234200,
+					"id_str": "1085206613005234177",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw9tqjQU8AE7mY1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw9tqjQU8AE7mY1.jpg",
+					"url": "https://t.co/7RUuMFdrol",
+					"display_url": "pic.twitter.com/7RUuMFdrol",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1085206615198916608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 407,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 613,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 613,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1085206613005234200,
+					"id_str": "1085206613005234177",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw9tqjQU8AE7mY1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw9tqjQU8AE7mY1.jpg",
+					"url": "https://t.co/7RUuMFdrol",
+					"display_url": "pic.twitter.com/7RUuMFdrol",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1085206615198916608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 407,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 613,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 613,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://pragmaticcode.com/linky/\" rel=\"nofollow\">Linky for iOS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6283,
+			"friends_count": 364,
+			"listed_count": 379,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 851,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17945,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1398689493",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is a book worth reading: A Heart in a Body in the World https://t.co/xEH716VDHD #YAfiction https://t.co/7RUuMFdrol"
+	},
+	{
 		"created_at": "Sat Jan 12 17:07:13 +0000 2019",
 		"id": 1084134718843408400,
 		"id_str": "1084134718843408385",

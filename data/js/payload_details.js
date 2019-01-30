dcec0a37fe2d5cccc17609536ea2cdfb170e4b1e
bhwilkoff <benwilkoff@gmail.com>
2019-01-30 15:22:04 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17991,
-	"created_at": "Wed Jan 30 19:22:01 +0000 2019",
+	"tweets": 17992,
+	"created_at": "Wed Jan 30 22:22:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,175 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Wed Jan 30 22:20:17 +0000 2019",
+		"id": 1090736483206459400,
+		"id_str": "1090736483206459393",
+		"full_text": "@Thedroneraider @torchapp @LumaTouch @emulsioapp Yeah. You have no idea how much stabilization I had to do to just get it to that level of quality. If you have suggestions for doing AR in the future, I’m all ears!\n\nAnd, thank you!",
+		"truncated": false,
+		"display_text_range": [
+			49,
+			230
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Thedroneraider",
+					"name": "Steve",
+					"id": 960613041934135300,
+					"id_str": "960613041934135299",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "torchapp",
+					"name": "Torch AR",
+					"id": 822860723223023600,
+					"id_str": "822860723223023616",
+					"indices": [
+						16,
+						25
+					]
+				},
+				{
+					"screen_name": "LumaTouch",
+					"name": "Luma Touch 📱🎞",
+					"id": 1194866647,
+					"id_str": "1194866647",
+					"indices": [
+						26,
+						36
+					]
+				},
+				{
+					"screen_name": "emulsioapp",
+					"name": "Emulsio",
+					"id": 263038916,
+					"id_str": "263038916",
+					"indices": [
+						37,
+						48
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1090735580940566500,
+		"in_reply_to_status_id_str": "1090735580940566528",
+		"in_reply_to_user_id": 960613041934135300,
+		"in_reply_to_user_id_str": "960613041934135299",
+		"in_reply_to_screen_name": "Thedroneraider",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6285,
+			"friends_count": 439,
+			"listed_count": 377,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 855,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17955,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1398689493",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "b49b3053b5c25bf5",
+			"url": "https://api.twitter.com/1.1/geo/id/b49b3053b5c25bf5.json",
+			"place_type": "city",
+			"name": "Denver",
+			"full_name": "Denver, CO",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-105.109815,
+							39.614151
+						],
+						[
+							-104.734372,
+							39.614151
+						],
+						[
+							-104.734372,
+							39.812975
+						],
+						[
+							-105.109815,
+							39.812975
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Thedroneraider @torchapp @LumaTouch @emulsioapp Yeah. You have no idea how much stabilization I had to do to just get it to that level of quality. If you have suggestions for doing AR in the future, I’m all ears!\n\nAnd, thank you!"
+	},
+	{
 		"created_at": "Wed Jan 30 18:35:40 +0000 2019",
 		"id": 1090679958517633000,
 		"id_str": "1090679958517633025",

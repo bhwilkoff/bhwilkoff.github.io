daed54a12198b99a74e52f24643eac09757fe120
bhwilkoff <benwilkoff@gmail.com>
2019-02-25 03:22:02 -0700

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -4723,6 +4723,112 @@ Grailbird.data.tweets_2019_02 =
 				}
 			},
 			"protected": false,
+			"followers_count": 153,
+			"friends_count": 53,
+			"listed_count": 34,
+			"created_at": "Wed Mar 11 22:34:43 +0000 2009",
+			"favourites_count": 530,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1122,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/621674936076079104/wX9ozsa9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/621674936076079104/wX9ozsa9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/23844531/1437053951",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Check out my new blog post - https://t.co/9JE73dd8iR"
+	},
+	{
+		"created_at": "Fri Feb 22 02:14:17 +0000 2019",
+		"id": 1098767906597261300,
+		"id_str": "1098767906597261313",
+		"full_text": "Check out my new blog post - https://t.co/9JE73dd8iR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			52
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9JE73dd8iR",
+					"expanded_url": "https://goo.gl/kLyfXP",
+					"display_url": "goo.gl/kLyfXP",
+					"indices": [
+						29,
+						52
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 23844531,
+			"id_str": "23844531",
+			"name": "Paige Dersham",
+			"screen_name": "BlueFrogPD",
+			"location": "Denver",
+			"description": "Science teacher, science geek, maker, mom, biker, costumer.",
+			"url": "http://t.co/uVxiOvQGgj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/uVxiOvQGgj",
+							"expanded_url": "http://www.paigedersham.com",
+							"display_url": "paigedersham.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 152,
 			"friends_count": 53,
 			"listed_count": 34,

32ed5d61bf34b2c90fecf3fe5fa9f7e8692efd0a
bhwilkoff <benwilkoff@gmail.com>
2019-03-27 12:22:02 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18239,
-	"created_at": "Tue Mar 19 19:22:01 +0000 2019",
+	"tweets": 18240,
+	"created_at": "Wed Mar 27 18:22:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 27 18:15:24 +0000 2019",
+		"id": 1110968580084908000,
+		"id_str": "1110968580084908034",
+		"full_text": "Is there something stronger than just writing #Truth? If so, #WhateverIsStrongerThanTruth for this: Why Do We Murder the Beautiful Friendships of Boys? https://t.co/IP9Vag5z07 https://t.co/sTLIYLEI1G",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Truth",
+					"indices": [
+						46,
+						52
+					]
+				},
+				{
+					"text": "WhateverIsStrongerThanTruth",
+					"indices": [
+						61,
+						89
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IP9Vag5z07",
+					"expanded_url": "https://medium.com/s/man-interrupted/why-do-we-murder-the-beautiful-friendships-of-boys-3ad722942755",
+					"display_url": "medium.com/s/man-interrup…",
+					"indices": [
+						152,
+						175
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1110968577740415000,
+					"id_str": "1110968577740414976",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/media/D2r0CQjWoAAcb4S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2r0CQjWoAAcb4S.jpg",
+					"url": "https://t.co/sTLIYLEI1G",
+					"display_url": "pic.twitter.com/sTLIYLEI1G",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1110968580084908034/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 842,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 559,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 842,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1110968577740415000,
+					"id_str": "1110968577740414976",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/media/D2r0CQjWoAAcb4S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2r0CQjWoAAcb4S.jpg",
+					"url": "https://t.co/sTLIYLEI1G",
+					"display_url": "pic.twitter.com/sTLIYLEI1G",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1110968580084908034/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 842,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 559,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 842,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://pragmaticcode.com/linky/\" rel=\"nofollow\">Linky for iOS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6377,
+			"friends_count": 1021,
+			"listed_count": 377,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 898,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18045,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1398689493",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Is there something stronger than just writing #Truth? If so, #WhateverIsStrongerThanTruth for this: Why Do We Murder the Beautiful Friendships of Boys? https://t.co/IP9Vag5z07 https://t.co/sTLIYLEI1G"
+	},
+	{
 		"created_at": "Tue Mar 19 19:07:52 +0000 2019",
 		"id": 1108082678891372500,
 		"id_str": "1108082678891372544",

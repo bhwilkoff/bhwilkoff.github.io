6b3fbb622df2ce50fc76c77a91bab4db461bf053
bhwilkoff <benwilkoff@gmail.com>
2019-05-02 10:22:04 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18263,
-	"created_at": "Wed May 01 19:22:01 +0000 2019",
+	"tweets": 18264,
+	"created_at": "Thu May 02 16:22:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 02 15:40:01 +0000 2019",
+		"id": 1123975437233860600,
+		"id_str": "1123975437233860608",
+		"full_text": "@nathanc @marnixst360 @torchapp @tinkercad @vectornator @Dropbox @Sketchfab I hadn't seen this example, nor had I thought of just pulling up @tinkercad in browser on the iPad. Great idea! I was playing with the newly released @polydust yesterday, which may be a good option in this workflow too!",
+		"truncated": false,
+		"display_text_range": [
+			76,
+			295
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nathanc",
+					"name": "Nathan C Bowser",
+					"id": 9317292,
+					"id_str": "9317292",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "marnixst360",
+					"name": "XRealities (martijn stellingwerff)",
+					"id": 742367938070601700,
+					"id_str": "742367938070601728",
+					"indices": [
+						9,
+						21
+					]
+				},
+				{
+					"screen_name": "torchapp",
+					"name": "Torch AR",
+					"id": 822860723223023600,
+					"id_str": "822860723223023616",
+					"indices": [
+						22,
+						31
+					]
+				},
+				{
+					"screen_name": "tinkercad",
+					"name": "Tinkercad",
+					"id": 90323375,
+					"id_str": "90323375",
+					"indices": [
+						32,
+						42
+					]
+				},
+				{
+					"screen_name": "vectornator",
+					"name": "Vectornator",
+					"id": 855873183244800000,
+					"id_str": "855873183244800000",
+					"indices": [
+						43,
+						55
+					]
+				},
+				{
+					"screen_name": "Dropbox",
+					"name": "Dropbox",
+					"id": 14749606,
+					"id_str": "14749606",
+					"indices": [
+						56,
+						64
+					]
+				},
+				{
+					"screen_name": "Sketchfab",
+					"name": "Sketchfab",
+					"id": 421923426,
+					"id_str": "421923426",
+					"indices": [
+						65,
+						75
+					]
+				},
+				{
+					"screen_name": "tinkercad",
+					"name": "Tinkercad",
+					"id": 90323375,
+					"id_str": "90323375",
+					"indices": [
+						141,
+						151
+					]
+				},
+				{
+					"screen_name": "polydust",
+					"name": "Polydust",
+					"id": 795400629992779800,
+					"id_str": "795400629992779776",
+					"indices": [
+						226,
+						235
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://twitterrific.com/mac\" rel=\"nofollow\">Twitterrific for Mac</a>",
+		"in_reply_to_status_id": 1123970802083299300,
+		"in_reply_to_status_id_str": "1123970802083299328",
+		"in_reply_to_user_id": 9317292,
+		"in_reply_to_user_id_str": "9317292",
+		"in_reply_to_screen_name": "nathanc",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6403,
+			"friends_count": 1217,
+			"listed_count": 378,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 913,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18069,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1398689493",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@nathanc @marnixst360 @torchapp @tinkercad @vectornator @Dropbox @Sketchfab I hadn't seen this example, nor had I thought of just pulling up @tinkercad in browser on the iPad. Great idea! I was playing with the newly released @polydust yesterday, which may be a good option in this workflow too!"
+	},
+	{
 		"created_at": "Wed May 01 19:12:34 +0000 2019",
 		"id": 1123666538555367400,
 		"id_str": "1123666538555367424",

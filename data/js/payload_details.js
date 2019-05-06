6fc72cb3656c7637221f60d3da8f403ca91fa04b
bhwilkoff <benwilkoff@gmail.com>
2019-05-06 15:22:03 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18272,
-	"created_at": "Mon May 06 18:22:00 +0000 2019",
+	"tweets": 18273,
+	"created_at": "Mon May 06 21:22:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,302 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Mon May 06 20:56:56 +0000 2019",
+		"id": 1125504745311838200,
+		"id_str": "1125504745311838208",
+		"full_text": "Special thanks to @taykendesign for organizing an inspiring conversation around donuts last week. Happy to hang with @jennyfilipetti @bwholmquist @stanley_gould @jonzornow @sophiamstrickfa @annobermann @dukecrawford and Rebecca Reid any time! #TaykenOnTheTown https://t.co/gmUHCM9js8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TaykenOnTheTown",
+					"indices": [
+						243,
+						259
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "taykendesign",
+					"name": "𝕋𝕒𝕪𝕝𝕠𝕣 𝕂𝕖𝕟𝕕𝕒𝕝",
+					"id": 716093571900649500,
+					"id_str": "716093571900649473",
+					"indices": [
+						18,
+						31
+					]
+				},
+				{
+					"screen_name": "jennyfilipetti",
+					"name": "jenny filipetti",
+					"id": 1035550592,
+					"id_str": "1035550592",
+					"indices": [
+						117,
+						132
+					]
+				},
+				{
+					"screen_name": "bwholmquist",
+					"name": "Ben Holmquist",
+					"id": 725425226,
+					"id_str": "725425226",
+					"indices": [
+						133,
+						145
+					]
+				},
+				{
+					"screen_name": "stanley_gould",
+					"name": "Stanley Gould",
+					"id": 3222738656,
+					"id_str": "3222738656",
+					"indices": [
+						146,
+						160
+					]
+				},
+				{
+					"screen_name": "jonzornow",
+					"name": "Jon Zornow 🚀",
+					"id": 111508605,
+					"id_str": "111508605",
+					"indices": [
+						161,
+						171
+					]
+				},
+				{
+					"screen_name": "sophiamstrickfa",
+					"name": "Sophia Strickfaden",
+					"id": 3824765727,
+					"id_str": "3824765727",
+					"indices": [
+						172,
+						188
+					]
+				},
+				{
+					"screen_name": "annobermann",
+					"name": "Ann Obermann",
+					"id": 3012716340,
+					"id_str": "3012716340",
+					"indices": [
+						189,
+						201
+					]
+				},
+				{
+					"screen_name": "dukecrawford",
+					"name": "Duke Crawford",
+					"id": 17845080,
+					"id_str": "17845080",
+					"indices": [
+						202,
+						215
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1125504737837703200,
+					"id_str": "1125504737837703168",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/D56YmIvV4AAuFBL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D56YmIvV4AAuFBL.jpg",
+					"url": "https://t.co/gmUHCM9js8",
+					"display_url": "pic.twitter.com/gmUHCM9js8",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1125504745311838208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1125504737837703200,
+					"id_str": "1125504737837703168",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/D56YmIvV4AAuFBL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D56YmIvV4AAuFBL.jpg",
+					"url": "https://t.co/gmUHCM9js8",
+					"display_url": "pic.twitter.com/gmUHCM9js8",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1125504745311838208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6405,
+			"friends_count": 1244,
+			"listed_count": 378,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 916,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18081,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1398689493",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "b49b3053b5c25bf5",
+			"url": "https://api.twitter.com/1.1/geo/id/b49b3053b5c25bf5.json",
+			"place_type": "city",
+			"name": "Denver",
+			"full_name": "Denver, CO",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-105.109815,
+							39.614151
+						],
+						[
+							-104.734372,
+							39.614151
+						],
+						[
+							-104.734372,
+							39.812975
+						],
+						[
+							-105.109815,
+							39.812975
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Special thanks to @taykendesign for organizing an inspiring conversation around donuts last week. Happy to hang with @jennyfilipetti @bwholmquist @stanley_gould @jonzornow @sophiamstrickfa @annobermann @dukecrawford and Rebecca Reid any time! #TaykenOnTheTown https://t.co/gmUHCM9js8"
+	},
+	{
 		"created_at": "Mon May 06 18:17:35 +0000 2019",
 		"id": 1125464643307073500,
 		"id_str": "1125464643307073538",

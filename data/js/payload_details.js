9c2318e6acebad0d6cca103e6ab757cdd15097fe
bhwilkoff <benwilkoff@gmail.com>
2019-05-20 09:22:06 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18354,
-	"created_at": "Mon May 20 12:22:02 +0000 2019",
+	"tweets": 18355,
+	"created_at": "Mon May 20 15:22:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Mon May 20 14:37:28 +0000 2019",
+		"id": 1130482677658730500,
+		"id_str": "1130482677658730497",
+		"full_text": "@mweinraub87 @amichaelberman @Bali_Maha @Dan_Blick @tomliamlynch @barnillas @calimorrison @mzrascoff @jlknott @lindseyedixon Yep. Playing around /w language is something we tend to love in education. And, language we choose says something about the spaces we choose to inhabit and the pedagogies we empower/allow. When I say IRL, I prize human connection and I think critically about where it happens.",
+		"truncated": false,
+		"display_text_range": [
+			125,
+			401
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mweinraub87",
+					"name": "Michael Weinraub",
+					"id": 972524358,
+					"id_str": "972524358",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "amichaelberman",
+					"name": "A. Michael Berman",
+					"id": 16159736,
+					"id_str": "16159736",
+					"indices": [
+						13,
+						28
+					]
+				},
+				{
+					"screen_name": "Bali_Maha",
+					"name": "ℳąhą Bąℓi, PhD مها بالي 🏵",
+					"id": 1535273520,
+					"id_str": "1535273520",
+					"indices": [
+						29,
+						39
+					]
+				},
+				{
+					"screen_name": "Dan_Blick",
+					"name": "Dan Blickensderfer",
+					"id": 188554158,
+					"id_str": "188554158",
+					"indices": [
+						40,
+						50
+					]
+				},
+				{
+					"screen_name": "tomliamlynch",
+					"name": "Gradgrind's",
+					"id": 16987591,
+					"id_str": "16987591",
+					"indices": [
+						51,
+						64
+					]
+				},
+				{
+					"screen_name": "barnillas",
+					"name": "Beatriz Arnillas",
+					"id": 65504675,
+					"id_str": "65504675",
+					"indices": [
+						65,
+						75
+					]
+				},
+				{
+					"screen_name": "calimorrison",
+					"name": "Dr. Cali Morrison (DocCMo bc it's fun)",
+					"id": 22952939,
+					"id_str": "22952939",
+					"indices": [
+						76,
+						89
+					]
+				},
+				{
+					"screen_name": "mzrascoff",
+					"name": "Matthew Rascoff",
+					"id": 9469272,
+					"id_str": "9469272",
+					"indices": [
+						90,
+						100
+					]
+				},
+				{
+					"screen_name": "jlknott",
+					"name": "Dr. Jessica L. Knott",
+					"id": 7693002,
+					"id_str": "7693002",
+					"indices": [
+						101,
+						109
+					]
+				},
+				{
+					"screen_name": "lindseyedixon",
+					"name": "Lindsey E. Dixon",
+					"id": 107295639,
+					"id_str": "107295639",
+					"indices": [
+						110,
+						124
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://twitterrific.com/mac\" rel=\"nofollow\">Twitterrific for Mac</a>",
+		"in_reply_to_status_id": 1130480730499420200,
+		"in_reply_to_status_id_str": "1130480730499420160",
+		"in_reply_to_user_id": 972524358,
+		"in_reply_to_user_id_str": "972524358",
+		"in_reply_to_screen_name": "mweinraub87",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6442,
+			"friends_count": 1372,
+			"listed_count": 378,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 950,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18177,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1017879115565678592/9rs-vqdB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1398689493",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@mweinraub87 @amichaelberman @Bali_Maha @Dan_Blick @tomliamlynch @barnillas @calimorrison @mzrascoff @jlknott @lindseyedixon Yep. Playing around /w language is something we tend to love in education. And, language we choose says something about the spaces we choose to inhabit and the pedagogies we empower/allow. When I say IRL, I prize human connection and I think critically about where it happens."
+	},
+	{
 		"created_at": "Mon May 20 11:35:54 +0000 2019",
 		"id": 1130436985800183800,
 		"id_str": "1130436985800183814",

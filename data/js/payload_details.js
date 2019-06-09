612025d37111bf511e60d37b1806cadcd84e5955
bhwilkoff <benwilkoff@gmail.com>
2019-06-09 15:22:02 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18394,
-	"created_at": "Sun Jun 09 02:22:00 +0000 2019",
+	"tweets": 18395,
+	"created_at": "Sun Jun 09 21:22:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,154 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Sun Jun 09 21:11:59 +0000 2019",
+		"id": 1137760342417612800,
+		"id_str": "1137829720395935744",
+		"full_text": "Excited to be on my way to Keystone and @InnEdCO to share a keynote with @jessifer. It will be great: 1. To be back in Colorado; 2. To meet and collaborate with #K12 teachers from all over the state! #digped #InnEdCO19",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			218
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "K12",
+					"indices": [
+						161,
+						165
+					]
+				},
+				{
+					"text": "digped",
+					"indices": [
+						200,
+						207
+					]
+				},
+				{
+					"text": "InnEdCO19",
+					"indices": [
+						208,
+						218
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "InnEdCO",
+					"name": "InnEdCO",
+					"id": 103948271,
+					"id_str": "103948271",
+					"indices": [
+						40,
+						48
+					]
+				},
+				{
+					"screen_name": "Jessifer",
+					"name": "Jesse Stommel",
+					"id": 11702102,
+					"id_str": "11702102",
+					"indices": [
+						73,
+						82
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 565218468,
+			"id_str": "565218468",
+			"name": "Sean Michael Morris",
+			"screen_name": "slamteacher",
+			"location": "Portland, OR",
+			"description": "Agonist. Critical Digital Pedagogy. Director: @DigPedLab. Author: An Urgency of Teachers (https://t.co/R92CXp9X00). Married to @MattMorris. He/him. #digped",
+			"url": "https://t.co/KOiKNx8wVK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KOiKNx8wVK",
+							"expanded_url": "http://www.seanmichaelmorris.com",
+							"display_url": "seanmichaelmorris.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/R92CXp9X00",
+							"expanded_url": "http://urgencyofteachers.com",
+							"display_url": "urgencyofteachers.com",
+							"indices": [
+								90,
+								113
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 6992,
+			"friends_count": 1254,
+			"listed_count": 362,
+			"created_at": "Sat Apr 28 03:52:49 +0000 2012",
+			"favourites_count": 12302,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20261,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "8A6915",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/963501917866307584/bPbRqJzR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/963501917866307584/bPbRqJzR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/565218468/1536805765",
+			"profile_link_color": "0B9E10",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 12,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Excited to be on my way to Keystone and @InnEdCO to share a keynote with @jessifer. It will be great: 1. To be back in Colorado; 2. To meet and collaborate with #K12 teachers from all over the state! #digped #InnEdCO19"
+	},
+	{
 		"created_at": "Sun Jun 09 02:20:01 +0000 2019",
 		"id": 1137527451263782900,
 		"id_str": "1137544848930627584",

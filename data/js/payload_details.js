ba2490b0cbc648d02d20bd497894bb9a2db9a9e0
bhwilkoff <benwilkoff@gmail.com>
2019-06-11 11:22:02 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18413,
-	"created_at": "Tue Jun 11 16:22:01 +0000 2019",
+	"tweets": 18414,
+	"created_at": "Tue Jun 11 17:22:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Tue Jun 11 17:21:17 +0000 2019",
+		"id": 1138495378125312000,
+		"id_str": "1138496435979202561",
+		"full_text": "My “why”?\n\nNo student should be told “you’re too dumb for college” and tracked out—like I was as a teenager in Michigan. Every student needs a champion in the system.\n\nEvery student has a path and destination in higher education—2 year, 4 year, or graduate.\n\n#InnEdCO19",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			269
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "InnEdCO19",
+					"indices": [
+						259,
+						269
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3432471664,
+			"id_str": "3432471664",
+			"name": "Matthew C Henry",
+			"screen_name": "fatpandasensei",
+			"location": "Cary, NC",
+			"description": "Fat. Geek. Technoshaman. Apple Monk. Windows10 Footsoldier. Urban Buddhist. Destroyer of Buffets. Bleeds Sriracha. Never opinionless. Skidoosh. WTCC Program Dir",
+			"url": "https://t.co/Wgzg1P2MZr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Wgzg1P2MZr",
+							"expanded_url": "http://fatpandasensei.tumblr.com",
+							"display_url": "fatpandasensei.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 67,
+			"friends_count": 127,
+			"listed_count": 6,
+			"created_at": "Thu Aug 20 05:10:49 +0000 2015",
+			"favourites_count": 82,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 326,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/684509562930511872/TFdOJZbu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/684509562930511872/TFdOJZbu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3432471664/1452034793",
+			"profile_link_color": "4A913C",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "e21c8e4914eef2b3",
+			"url": "https://api.twitter.com/1.1/geo/id/e21c8e4914eef2b3.json",
+			"place_type": "admin",
+			"name": "Colorado",
+			"full_name": "Colorado, USA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-109.060257,
+							36.992427
+						],
+						[
+							-102.041524,
+							36.992427
+						],
+						[
+							-102.041524,
+							41.003445
+						],
+						[
+							-109.060257,
+							41.003445
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "My “why”?\n\nNo student should be told “you’re too dumb for college” and tracked out—like I was as a teenager in Michigan. Every student needs a champion in the system.\n\nEvery student has a path and destination in higher education—2 year, 4 year, or graduate.\n\n#InnEdCO19"
+	},
+	{
 		"created_at": "Tue Jun 11 15:34:11 +0000 2019",
 		"id": 1138469486011355100,
 		"id_str": "1138469486011355136",

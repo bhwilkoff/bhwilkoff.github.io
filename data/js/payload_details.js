afefd0ccb38f342463543d66f4f06baf101acfd0
bhwilkoff <benwilkoff@gmail.com>
2019-06-27 09:22:02 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18436,
-	"created_at": "Tue Jun 25 19:22:01 +0000 2019",
+	"tweets": 18437,
+	"created_at": "Thu Jun 27 15:22:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,255 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Thu Jun 27 15:17:30 +0000 2019",
+		"id": 1144263492079407100,
+		"id_str": "1144263492079407118",
+		"full_text": "It isn't often someone solves a problem I have in a code snippet tweet, but when they do, it is awesome! https://t.co/pOUeAGO3Zc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pOUeAGO3Zc",
+					"expanded_url": "https://twitter.com/JimMcKeeth/status/1084603280292888576",
+					"display_url": "twitter.com/JimMcKeeth/sta…",
+					"indices": [
+						105,
+						128
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6494,
+			"friends_count": 1719,
+			"listed_count": 379,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1045,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18277,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1398689493",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1084603280292888600,
+		"quoted_status_id_str": "1084603280292888576",
+		"quoted_status_permalink": {
+			"url": "https://t.co/pOUeAGO3Zc",
+			"expanded": "https://twitter.com/JimMcKeeth/status/1084603280292888576",
+			"display": "twitter.com/JimMcKeeth/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jan 14 00:09:07 +0000 2019",
+			"id": 1084603280292888600,
+			"id_str": "1084603280292888576",
+			"full_text": "Want the URL of the first search result in a Google sheet? =SUBSTITUTE(SUBSTITUTE(SUBSTITUTE(SUBSTITUTE(index(IMPORTXML(\"https://t.co/3XKFfCErvb\"&amp;A1,\"//a[@class='result__a']/@href\"),1),\"/l/?kh=-1&amp;uddg=\",\"\"),\"%3A\",\":\"),\"%2F\",\"/\"),\"%2D\",\"-\")",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				247
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Class",
+						"name": "Eli Fabens",
+						"id": 6727282,
+						"id_str": "6727282",
+						"indices": [
+							158,
+							164
+						]
+					},
+					{
+						"screen_name": "href",
+						"name": "href",
+						"id": 14477654,
+						"id_str": "14477654",
+						"indices": [
+							178,
+							183
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/3XKFfCErvb",
+						"expanded_url": "https://duckduckgo.com/?q=",
+						"display_url": "duckduckgo.com/?q=",
+						"indices": [
+							121,
+							144
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15181596,
+				"id_str": "15181596",
+				"name": "Jim McKeeth ❤️ Delphi 🔥🐒🤖🍎",
+				"screen_name": "JimMcKeeth",
+				"location": "Nampa, ID",
+				"description": "Developer Advocate and Engineer at Embarcadero Technologies and host of the Podcast at https://t.co/mDuJ19wwA0. Improvisor, Author, Speaker, Inventor, and Dad.",
+				"url": "https://t.co/sqOcLWHBHW",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/sqOcLWHBHW",
+								"expanded_url": "http://www.delphi.org",
+								"display_url": "delphi.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/mDuJ19wwA0",
+								"expanded_url": "http://Delphi.org",
+								"display_url": "Delphi.org",
+								"indices": [
+									87,
+									110
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 3531,
+				"friends_count": 931,
+				"listed_count": 161,
+				"created_at": "Fri Jun 20 16:38:37 +0000 2008",
+				"favourites_count": 1658,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 14346,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1105277742981963778/TmI3jS6g_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1105277742981963778/TmI3jS6g_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15181596/1416272492",
+				"profile_link_color": "FF0000",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It isn't often someone solves a problem I have in a code snippet tweet, but when they do, it is awesome! https://t.co/pOUeAGO3Zc"
+	},
+	{
 		"created_at": "Tue Jun 25 18:38:00 +0000 2019",
 		"id": 1143589174399733800,
 		"id_str": "1143589174399733760",

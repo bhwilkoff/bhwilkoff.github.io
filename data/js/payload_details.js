dd697a775b331412575e3fbc6ebfe8bec4193363
bhwilkoff <benwilkoff@gmail.com>
2019-07-01 12:22:02 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18437,
-	"created_at": "Thu Jun 27 15:22:01 +0000 2019",
+	"tweets": 18438,
+	"created_at": "Mon Jul 01 18:22:01 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_07.js
@@ -0,0 +1,407 @@
+Grailbird.data.tweets_2019_07 = 
+[
+	{
+		"created_at": "Mon Jul 01 17:37:13 +0000 2019",
+		"id": 1145748202487816200,
+		"id_str": "1145748202487816192",
+		"full_text": "Great reminder. Thank you @FW_Medieval. I’ll only add that conferencing is made far easier (for me) when I try to make meaning as I go. #ConferencingIsHard https://t.co/tvoRZIEfW9 https://t.co/c7unjxoa9z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			179
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ConferencingIsHard",
+					"indices": [
+						136,
+						155
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "FW_Medieval",
+					"name": "Damian Fleming 🙏",
+					"id": 207972915,
+					"id_str": "207972915",
+					"indices": [
+						26,
+						38
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/tvoRZIEfW9",
+					"expanded_url": "https://twitter.com/FW_Medieval/status/1145743943595909120",
+					"display_url": "twitter.com/FW_Medieval/st…",
+					"indices": [
+						156,
+						179
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1145748168384008200,
+					"id_str": "1145748168384008192",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/D-aD44KVUAAUgtu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-aD44KVUAAUgtu.jpg",
+					"url": "https://t.co/c7unjxoa9z",
+					"display_url": "pic.twitter.com/c7unjxoa9z",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1145748202487816192/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1777,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1145748168384008200,
+					"id_str": "1145748168384008192",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/D-aD44KVUAAUgtu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-aD44KVUAAUgtu.jpg",
+					"url": "https://t.co/c7unjxoa9z",
+					"display_url": "pic.twitter.com/c7unjxoa9z",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1145748202487816192/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1777,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1145748168736292900,
+					"id_str": "1145748168736292865",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/D-aD45eUwAEoCF6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-aD45eUwAEoCF6.jpg",
+					"url": "https://t.co/c7unjxoa9z",
+					"display_url": "pic.twitter.com/c7unjxoa9z",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1145748202487816192/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1145748169260593200,
+					"id_str": "1145748169260593152",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/D-aD47bU8AAucnw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-aD47bU8AAucnw.jpg",
+					"url": "https://t.co/c7unjxoa9z",
+					"display_url": "pic.twitter.com/c7unjxoa9z",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1145748202487816192/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1145743943595909100,
+		"in_reply_to_status_id_str": "1145743943595909120",
+		"in_reply_to_user_id": 207972915,
+		"in_reply_to_user_id_str": "207972915",
+		"in_reply_to_screen_name": "FW_Medieval",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6489,
+			"friends_count": 1725,
+			"listed_count": 379,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1045,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18278,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1398689493",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "b49b3053b5c25bf5",
+			"url": "https://api.twitter.com/1.1/geo/id/b49b3053b5c25bf5.json",
+			"place_type": "city",
+			"name": "Denver",
+			"full_name": "Denver, CO",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-105.109815,
+							39.614151
+						],
+						[
+							-104.734372,
+							39.614151
+						],
+						[
+							-104.734372,
+							39.812975
+						],
+						[
+							-105.109815,
+							39.812975
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1145743943595909100,
+		"quoted_status_id_str": "1145743943595909120",
+		"quoted_status_permalink": {
+			"url": "https://t.co/tvoRZIEfW9",
+			"expanded": "https://twitter.com/FW_Medieval/status/1145743943595909120",
+			"display": "twitter.com/FW_Medieval/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jul 01 17:20:17 +0000 2019",
+			"id": 1145743943595909100,
+			"id_str": "1145743943595909120",
+			"full_text": "Hey everybody, but especially all my friends at #IMC2019 right now!  I just wanna remind everybody that there is no wrong way to conference. There's no right way you should be feeling. Conferences can be extremely overwhelming. It's fine to go to sessions. It's fine to skip+",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				275
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "IMC2019",
+						"indices": [
+							48,
+							56
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 207972915,
+				"id_str": "207972915",
+				"name": "Damian Fleming 🙏",
+				"screen_name": "FW_Medieval",
+				"location": "United States",
+				"description": "Medievalist & Latinist @ Fort Wayne : pseudo-philologist ł wannabe paleographer ł aspirational hebraist א he/his/him // Senior Fellow, VP @SoFCritBibRBS",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 7308,
+				"friends_count": 2672,
+				"listed_count": 183,
+				"created_at": "Tue Oct 26 11:48:50 +0000 2010",
+				"favourites_count": 102590,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 69610,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "835C3B",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1124028208658169856/lRnvoWdQ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1124028208658169856/lRnvoWdQ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/207972915/1556665449",
+				"profile_link_color": "FA743E",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 25,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Great reminder. Thank you @FW_Medieval. I’ll only add that conferencing is made far easier (for me) when I try to make meaning as I go. #ConferencingIsHard https://t.co/tvoRZIEfW9 https://t.co/c7unjxoa9z"
+	}
+]

989252adbaf0549e801645a1f601b09692b399b6
bhwilkoff <benwilkoff@gmail.com>
2019-08-08 21:22:01 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18462,
-	"created_at": "Thu Aug 08 21:22:01 +0000 2019",
+	"tweets": 18463,
+	"created_at": "Fri Aug 09 03:22:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 09 02:53:34 +0000 2019",
+		"id": 1159658953912926200,
+		"id_str": "1159658953912926208",
+		"full_text": "I really want to read this guy’s new book, but it doesn’t come out until September. ;) Is Big Tech addictive? A debate with Nir Eyal. https://t.co/F0b1yM4qEG https://t.co/L1vGgeUk9G",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			157
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/F0b1yM4qEG",
+					"expanded_url": "https://www.vox.com/podcasts/2019/8/7/20750214/nir-eyal-tech-addiction-ezra-klein-smartphones-hooked-indistractable",
+					"display_url": "vox.com/podcasts/2019/…",
+					"indices": [
+						134,
+						157
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1159658950914015200,
+					"id_str": "1159658950914015238",
+					"indices": [
+						158,
+						181
+					],
+					"media_url": "http://pbs.twimg.com/media/EBfvq_XVAAY6vSN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBfvq_XVAAY6vSN.jpg",
+					"url": "https://t.co/L1vGgeUk9G",
+					"display_url": "pic.twitter.com/L1vGgeUk9G",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1159658953912926208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 865,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 865,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 574,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1159658950914015200,
+					"id_str": "1159658950914015238",
+					"indices": [
+						158,
+						181
+					],
+					"media_url": "http://pbs.twimg.com/media/EBfvq_XVAAY6vSN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBfvq_XVAAY6vSN.jpg",
+					"url": "https://t.co/L1vGgeUk9G",
+					"display_url": "pic.twitter.com/L1vGgeUk9G",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1159658953912926208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 865,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 865,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 574,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://pragmaticcode.com/linky/\" rel=\"nofollow\">Linky for iOS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6498,
+			"friends_count": 1797,
+			"listed_count": 379,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1059,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18308,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I really want to read this guy’s new book, but it doesn’t come out until September. ;) Is Big Tech addictive? A debate with Nir Eyal. https://t.co/F0b1yM4qEG https://t.co/L1vGgeUk9G"
+	},
+	{
 		"created_at": "Thu Aug 08 20:33:57 +0000 2019",
 		"id": 1159563418833080300,
 		"id_str": "1159563418833080320",

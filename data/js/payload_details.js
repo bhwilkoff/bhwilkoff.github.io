534adf76ac2dac2163bd8d16b555fbd174871b5e
bhwilkoff <benwilkoff@gmail.com>
2019-09-01 08:22:01 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18473,
-	"created_at": "Sat Aug 31 17:22:00 +0000 2019",
+	"tweets": 18474,
+	"created_at": "Sun Sep 01 14:22:00 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_09.js
@@ -0,0 +1,187 @@
+Grailbird.data.tweets_2019_09 = 
+[
+	{
+		"created_at": "Sun Sep 01 14:20:08 +0000 2019",
+		"id": 1168166654066315300,
+		"id_str": "1168166654066315264",
+		"full_text": "This is how echo chambers work. Platforms that encourage the worst parts of community, create weapons out of ideas that cause measurable and observable harm. Algorithms are not agnostic.\n\nHow YouTube Radicalized Brazil https://t.co/Mi9mNXWN7b https://t.co/P0jWgEIeWk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			242
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Mi9mNXWN7b",
+					"expanded_url": "https://www.nytimes.com/2019/08/11/world/americas/youtube-brazil.html",
+					"display_url": "nytimes.com/2019/08/11/wor…",
+					"indices": [
+						219,
+						242
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1168166651730153500,
+					"id_str": "1168166651730153477",
+					"indices": [
+						243,
+						266
+					],
+					"media_url": "http://pbs.twimg.com/media/EDYpYWrVUAU3j7L.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDYpYWrVUAU3j7L.jpg",
+					"url": "https://t.co/P0jWgEIeWk",
+					"display_url": "pic.twitter.com/P0jWgEIeWk",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1168166654066315264/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 505,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 761,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 761,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1168166651730153500,
+					"id_str": "1168166651730153477",
+					"indices": [
+						243,
+						266
+					],
+					"media_url": "http://pbs.twimg.com/media/EDYpYWrVUAU3j7L.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDYpYWrVUAU3j7L.jpg",
+					"url": "https://t.co/P0jWgEIeWk",
+					"display_url": "pic.twitter.com/P0jWgEIeWk",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1168166654066315264/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 505,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 761,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 761,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://pragmaticcode.com/linky/\" rel=\"nofollow\">Linky for iOS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6500,
+			"friends_count": 1989,
+			"listed_count": 379,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1064,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18318,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is how echo chambers work. Platforms that encourage the worst parts of community, create weapons out of ideas that cause measurable and observable harm. Algorithms are not agnostic.\n\nHow YouTube Radicalized Brazil https://t.co/Mi9mNXWN7b https://t.co/P0jWgEIeWk"
+	}
+]

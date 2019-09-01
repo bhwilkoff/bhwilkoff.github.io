c669873da7ca1c356baeec8d2b63539072cd151a
bhwilkoff <benwilkoff@gmail.com>
2019-09-01 09:22:02 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18474,
-	"created_at": "Sun Sep 01 14:22:00 +0000 2019",
+	"tweets": 18475,
+	"created_at": "Sun Sep 01 15:22:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sun Sep 01 14:24:48 +0000 2019",
+		"id": 1168167828882509800,
+		"id_str": "1168167828882509824",
+		"full_text": "A good reminder for how my mind works to pursue my goals. Sometimes the tension I feel feels good. Sometimes it is overwhelming.\n\nA User’s Guide to Creative Tension https://t.co/XSyMgycqgK https://t.co/3XWVkTw77e",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			188
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XSyMgycqgK",
+					"expanded_url": "https://www.productiveflourishing.com/a-users-guide-to-creative-tension/",
+					"display_url": "productiveflourishing.com/a-users-guide-…",
+					"indices": [
+						165,
+						188
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1168167827611631600,
+					"id_str": "1168167827611631616",
+					"indices": [
+						189,
+						212
+					],
+					"media_url": "http://pbs.twimg.com/media/EDYqczLUwAAQF1s.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDYqczLUwAAQF1s.jpg",
+					"url": "https://t.co/3XWVkTw77e",
+					"display_url": "pic.twitter.com/3XWVkTw77e",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1168167828882509824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 667,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1004,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1004,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1168167827611631600,
+					"id_str": "1168167827611631616",
+					"indices": [
+						189,
+						212
+					],
+					"media_url": "http://pbs.twimg.com/media/EDYqczLUwAAQF1s.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDYqczLUwAAQF1s.jpg",
+					"url": "https://t.co/3XWVkTw77e",
+					"display_url": "pic.twitter.com/3XWVkTw77e",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1168167828882509824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 667,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1004,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1004,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://pragmaticcode.com/linky/\" rel=\"nofollow\">Linky for iOS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6500,
+			"friends_count": 1989,
+			"listed_count": 379,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1064,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18319,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A good reminder for how my mind works to pursue my goals. Sometimes the tension I feel feels good. Sometimes it is overwhelming.\n\nA User’s Guide to Creative Tension https://t.co/XSyMgycqgK https://t.co/3XWVkTw77e"
+	},
+	{
 		"created_at": "Sun Sep 01 14:20:08 +0000 2019",
 		"id": 1168166654066315300,
 		"id_str": "1168166654066315264",

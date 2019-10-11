35ee75b3418147c57350eb1d68b44fc91195c44f
bhwilkoff <benwilkoff@gmail.com>
2019-10-11 12:22:01 -0600

    Added 1 tweets

---
+++ data/js/tweets/2019_10.js
@@ -0,0 +1,209 @@
+Grailbird.data.tweets_2019_10 = 
+[
+	{
+		"created_at": "Fri Oct 11 17:31:59 +0000 2019",
+		"id": 1179947094972350500,
+		"id_str": "1182710448019259392",
+		"full_text": "Presentation proposals are officially open for the #InnEdCO2020 Conference! Go to https://t.co/n44lWj4WVV to share your adventure! #adventureawaits #InnEdCO https://t.co/NZmYIWygXM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			156
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "InnEdCO2020",
+					"indices": [
+						51,
+						63
+					]
+				},
+				{
+					"text": "adventureawaits",
+					"indices": [
+						131,
+						147
+					]
+				},
+				{
+					"text": "InnEdCO",
+					"indices": [
+						148,
+						156
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/n44lWj4WVV",
+					"expanded_url": "http://innedco.org/proposals",
+					"display_url": "innedco.org/proposals",
+					"indices": [
+						82,
+						105
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1179947088123068400,
+					"id_str": "1179947088123068417",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/EGADoAFVAAEuKv7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGADoAFVAAEuKv7.jpg",
+					"url": "https://t.co/NZmYIWygXM",
+					"display_url": "pic.twitter.com/NZmYIWygXM",
+					"expanded_url": "https://twitter.com/InnEdCO/status/1179947094972350464/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1179947088123068400,
+					"id_str": "1179947088123068417",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/EGADoAFVAAEuKv7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGADoAFVAAEuKv7.jpg",
+					"url": "https://t.co/NZmYIWygXM",
+					"display_url": "pic.twitter.com/NZmYIWygXM",
+					"expanded_url": "https://twitter.com/InnEdCO/status/1179947094972350464/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 103948271,
+			"id_str": "103948271",
+			"name": "InnEdCO",
+			"screen_name": "InnEdCO",
+			"location": "",
+			"description": "We provide year-round networks for educators that support engagement in sustainable digital age leadership, teaching, and learning. #InnEdCO #InnEdCO19",
+			"url": "https://t.co/8xCYNu1QQO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8xCYNu1QQO",
+							"expanded_url": "http://innedco.org",
+							"display_url": "innedco.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2297,
+			"friends_count": 783,
+			"listed_count": 68,
+			"created_at": "Mon Jan 11 19:36:47 +0000 2010",
+			"favourites_count": 985,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2049,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/560832218096885760/96-qg4IL_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/560832218096885760/96-qg4IL_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/103948271/1497300705",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 14,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Presentation proposals are officially open for the #InnEdCO2020 Conference! Go to https://t.co/n44lWj4WVV to share your adventure! #adventureawaits #InnEdCO https://t.co/NZmYIWygXM"
+	}
+]

e85787f46efc110fafd4cc3edb9506f3fe4ad645
bhwilkoff <benwilkoff@gmail.com>
2019-11-06 09:22:02 -0700

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Wed Nov 06 15:30:58 +0000 2019",
+		"id": 1192102080355287000,
+		"id_str": "1192102080355287040",
+		"full_text": "This is a point that is often not discussed as we pursue VR as a learning technology. While I think “violent” is not quite the word, it is important to consider the way that we “force students” to experience learning regardless of the medium for delivery. https://t.co/IeJU0lcE1B https://t.co/a5yr73693I",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IeJU0lcE1B",
+					"expanded_url": "https://www.edsurge.com/news/2019-10-31-virtual-reality-experiences-can-be-violent-and-intrusive-they-need-an-artist-s-touch",
+					"display_url": "edsurge.com/news/2019-10-3…",
+					"indices": [
+						256,
+						279
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1192102077482197000,
+					"id_str": "1192102077482196992",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/EIsyhgBU4AAvWUP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIsyhgBU4AAvWUP.jpg",
+					"url": "https://t.co/a5yr73693I",
+					"display_url": "pic.twitter.com/a5yr73693I",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1192102080355287040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 621,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1121,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1121,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1192102077482197000,
+					"id_str": "1192102077482196992",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/EIsyhgBU4AAvWUP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIsyhgBU4AAvWUP.jpg",
+					"url": "https://t.co/a5yr73693I",
+					"display_url": "pic.twitter.com/a5yr73693I",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1192102080355287040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 621,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1121,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1121,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://pragmaticcode.com/linky/\" rel=\"nofollow\">Linky for iOS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Program Manager of Digital Education & Academic Technologies\n@cuanschutz. I try to create at least one new thing every day. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6518,
+			"friends_count": 2151,
+			"listed_count": 378,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1072,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18324,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is a point that is often not discussed as we pursue VR as a learning technology. While I think “violent” is not quite the word, it is important to consider the way that we “force students” to experience learning regardless of the medium for delivery. https://t.co/IeJU0lcE1B https://t.co/a5yr73693I"
+	},
+	{
 		"created_at": "Tue Nov 05 12:13:21 +0000 2019",
 		"id": 1191689960371282000,
 		"id_str": "1191689960371281923",

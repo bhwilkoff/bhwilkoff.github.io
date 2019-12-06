5f5edd739235a1c3995188022b55f686e49250df
bhwilkoff <benwilkoff@gmail.com>
2019-12-05 19:22:02 -0700

    Added 1 tweets

--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Fri Dec 06 01:49:28 +0000 2019",
+		"id": 1202766979431452700,
+		"id_str": "1202766979431452672",
+		"full_text": "I feel compelled to say that the most recent @MBMBaM is one of my absolute favorites. Thank you @travismcelroy @JustinMcElroy and @griffinmcelroy (I know he isn’t there). You make me so happy. This is a great place to start if you have never listened: https://t.co/qXvy3epkuH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MBMBaM",
+					"name": "MBMBaM",
+					"id": 131976246,
+					"id_str": "131976246",
+					"indices": [
+						45,
+						52
+					]
+				},
+				{
+					"screen_name": "travismcelroy",
+					"name": "Travis McElroy, The Internet's Best Friend",
+					"id": 19844987,
+					"id_str": "19844987",
+					"indices": [
+						96,
+						110
+					]
+				},
+				{
+					"screen_name": "JustinMcElroy",
+					"name": "Justin “Hoops” McElroy",
+					"id": 16421419,
+					"id_str": "16421419",
+					"indices": [
+						111,
+						125
+					]
+				},
+				{
+					"screen_name": "griffinmcelroy",
+					"name": "Griffin's Cool 2018 Tweets",
+					"id": 16874875,
+					"id_str": "16874875",
+					"indices": [
+						130,
+						145
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qXvy3epkuH",
+					"expanded_url": "https://overcast.fm/+KT6erD4",
+					"display_url": "overcast.fm/+KT6erD4",
+					"indices": [
+						252,
+						275
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Senior Operations Manager for Admissions @MinervaSchools. Proud husband; father of 3 strange and beautiful children. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6511,
+			"friends_count": 2291,
+			"listed_count": 378,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1081,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18329,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "85c09b7b6a803faf",
+			"url": "https://api.twitter.com/1.1/geo/id/85c09b7b6a803faf.json",
+			"place_type": "city",
+			"name": "Southglenn",
+			"full_name": "Southglenn, CO",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-104.988399,
+							39.565951
+						],
+						[
+							-104.922825,
+							39.565951
+						],
+						[
+							-104.922825,
+							39.613203
+						],
+						[
+							-104.988399,
+							39.613203
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I feel compelled to say that the most recent @MBMBaM is one of my absolute favorites. Thank you @travismcelroy @JustinMcElroy and @griffinmcelroy (I know he isn’t there). You make me so happy. This is a great place to start if you have never listened: https://t.co/qXvy3epkuH"
+	},
+	{
 		"created_at": "Sun Dec 01 20:56:00 +0000 2019",
 		"id": 1201243572885844000,
 		"id_str": "1201243572885843968",

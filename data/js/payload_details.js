524a62cc7ac70fc1cd5991d194d99b0d7fb56861
bhwilkoff <benwilkoff@gmail.com>
2020-02-05 17:22:02 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18504,
-	"created_at": "Wed Feb 05 23:22:00 +0000 2020",
+	"tweets": 18505,
+	"created_at": "Thu Feb 06 00:22:00 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Wed Feb 05 23:27:03 +0000 2020",
+		"id": 1225199184510251000,
+		"id_str": "1225199184510251011",
+		"full_text": "@steven_aquino @jsnell I do think it is interesting to see the preferences from those who tend toward a touch-first experience. My kids love the swipe gestures on the siri remote. They even prefer to use the iPad/iPhone remote app/control center experience before they would use a different remote.",
+		"truncated": false,
+		"display_text_range": [
+			23,
+			298
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "steven_aquino",
+					"name": "Steven Aquino",
+					"id": 152115336,
+					"id_str": "152115336",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "jsnell",
+					"name": "Jason Snell",
+					"id": 784912,
+					"id_str": "784912",
+					"indices": [
+						15,
+						22
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1225197328744632300,
+		"in_reply_to_status_id_str": "1225197328744632320",
+		"in_reply_to_user_id": 152115336,
+		"in_reply_to_user_id_str": "152115336",
+		"in_reply_to_screen_name": "steven_aquino",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Senior Operations Manager for Admissions @MinervaSchools. Proud husband; father of 3 strange and beautiful children. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6489,
+			"friends_count": 2390,
+			"listed_count": 377,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1104,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18345,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@steven_aquino @jsnell I do think it is interesting to see the preferences from those who tend toward a touch-first experience. My kids love the swipe gestures on the siri remote. They even prefer to use the iPad/iPhone remote app/control center experience before they would use a different remote."
+	},
+	{
 		"created_at": "Wed Feb 05 23:18:01 +0000 2020",
 		"id": 1225196911390380000,
 		"id_str": "1225196911390380032",

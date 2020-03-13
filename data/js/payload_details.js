aa503df027e5df342672a0c51d37a850f8ba6189
bhwilkoff <benwilkoff@gmail.com>
2020-03-13 14:22:02 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18515,
-	"created_at": "Fri Mar 13 00:22:01 +0000 2020",
+	"tweets": 18516,
+	"created_at": "Fri Mar 13 20:22:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Fri Mar 13 20:15:50 +0000 2020",
+		"id": 1238559413549457400,
+		"id_str": "1238559413549457409",
+		"full_text": "If you want to hear from the true experts on how to support kids in online courses, this is a session worth attending next week! I cannot think of a better way to celebrate St. Patrick's day (immersed in the science of learning)! https://t.co/0NdoS0iMyo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			229
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0NdoS0iMyo",
+					"expanded_url": "https://twitter.com/minervaproject/status/1238557840890941440",
+					"display_url": "twitter.com/minervaproject…",
+					"indices": [
+						230,
+						253
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Senior Operations Manager for Admissions @MinervaSchools. Proud husband; father of 3 strange and beautiful children. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6482,
+			"friends_count": 2466,
+			"listed_count": 375,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1140,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18355,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1238557840890941400,
+		"quoted_status_id_str": "1238557840890941440",
+		"quoted_status_permalink": {
+			"url": "https://t.co/0NdoS0iMyo",
+			"expanded": "https://twitter.com/minervaproject/status/1238557840890941440",
+			"display": "twitter.com/minervaproject…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Mar 13 20:09:35 +0000 2020",
+			"id": 1238557840890941400,
+			"id_str": "1238557840890941440",
+			"full_text": "We are here to help institutions adapt to the coronavirus realities, and in response, will be hosting a webinar on March 17 at 1PM PST / 4PM EST to share our lessons on how to transform your class into a virtual classroom. Sign up today: https://t.co/DFHzxG4c6B",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				261
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/DFHzxG4c6B",
+						"expanded_url": "http://minervavirtualclassroom.splashthat.com",
+						"display_url": "minervavirtualclassroom.splashthat.com",
+						"indices": [
+							238,
+							261
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 541412759,
+				"id_str": "541412759",
+				"name": "Minerva Project",
+				"screen_name": "minervaproject",
+				"location": "San Francisco, CA",
+				"description": "Nurturing Critical Wisdom for the Sake of the World. \n\nFollow @MinervaSchools for the undergraduate and graduate experience.",
+				"url": "http://t.co/ya2CgtmCB5",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/ya2CgtmCB5",
+								"expanded_url": "http://www.minervaproject.com",
+								"display_url": "minervaproject.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5196,
+				"friends_count": 95,
+				"listed_count": 225,
+				"created_at": "Sat Mar 31 06:24:47 +0000 2012",
+				"favourites_count": 466,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 716,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "A0968C",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/507608889940914176/4UdFuzeL_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/507608889940914176/4UdFuzeL_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/541412759/1554746289",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "If you want to hear from the true experts on how to support kids in online courses, this is a session worth attending next week! I cannot think of a better way to celebrate St. Patrick's day (immersed in the science of learning)! https://t.co/0NdoS0iMyo"
+	},
+	{
 		"created_at": "Thu Mar 12 23:56:07 +0000 2020",
 		"id": 1238086631208476700,
 		"id_str": "1238252462341054465",

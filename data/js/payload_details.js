e36e2d811ef7bc221920303c3bbd4e0cfb678e96
bhwilkoff <benwilkoff@gmail.com>
2020-03-31 11:22:03 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18537,
-	"created_at": "Tue Mar 31 00:22:01 +0000 2020",
+	"tweets": 18538,
+	"created_at": "Tue Mar 31 17:22:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Tue Mar 31 16:54:17 +0000 2020",
+		"id": 1245031676012445700,
+		"id_str": "1245031676012445697",
+		"full_text": "@ThugRaccoons @Aikiwomannc Is this a response from some stereotypical white family 1954? Just folded my 45th load of the pandemic. Enjoy contributing the wellbeing of your family. (Also, I get that it is a joke. Funny. Not Funny.) https://t.co/4EgPOqk6hk",
+		"truncated": false,
+		"display_text_range": [
+			27,
+			230
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ThugRaccoons",
+					"name": "Son of Dad",
+					"id": 836281596,
+					"id_str": "836281596",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "Aikiwomannc",
+					"name": "Christina, mostly merciless from a distance 🦇👻🤘",
+					"id": 65674900,
+					"id_str": "65674900",
+					"indices": [
+						14,
+						26
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1245031609478209500,
+					"id_str": "1245031609478209538",
+					"indices": [
+						231,
+						254
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EUc9pwEU4AI5n7D.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EUc9pwEU4AI5n7D.jpg",
+					"url": "https://t.co/4EgPOqk6hk",
+					"display_url": "pic.twitter.com/4EgPOqk6hk",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1245031676012445697/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 512,
+							"h": 384,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 512,
+							"h": 384,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 512,
+							"h": 384,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245031609478209500,
+					"id_str": "1245031609478209538",
+					"indices": [
+						231,
+						254
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EUc9pwEU4AI5n7D.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EUc9pwEU4AI5n7D.jpg",
+					"url": "https://t.co/4EgPOqk6hk",
+					"display_url": "pic.twitter.com/4EgPOqk6hk",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1245031676012445697/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 512,
+							"h": 384,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 512,
+							"h": 384,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 512,
+							"h": 384,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							3
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EUc9pwEU4AI5n7D.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1244977781164949500,
+		"in_reply_to_status_id_str": "1244977781164949504",
+		"in_reply_to_user_id": 836281596,
+		"in_reply_to_user_id_str": "836281596",
+		"in_reply_to_screen_name": "ThugRaccoons",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Senior Operations Manager for Admissions @MinervaSchools. Proud husband; father of 3 strange and beautiful children. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6477,
+			"friends_count": 2607,
+			"listed_count": 372,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1253,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18383,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ThugRaccoons @Aikiwomannc Is this a response from some stereotypical white family 1954? Just folded my 45th load of the pandemic. Enjoy contributing the wellbeing of your family. (Also, I get that it is a joke. Funny. Not Funny.) https://t.co/4EgPOqk6hk"
+	},
+	{
 		"created_at": "Tue Mar 31 00:08:12 +0000 2020",
 		"id": 1244778487279317000,
 		"id_str": "1244778487279316993",

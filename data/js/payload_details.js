35689ee691f92aeaa831b6043fca90b7a982246a
bhwilkoff <benwilkoff@gmail.com>
2020-04-14 13:22:02 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18579,
-	"created_at": "Tue Apr 14 17:22:01 +0000 2020",
+	"tweets": 18580,
+	"created_at": "Tue Apr 14 19:22:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Tue Apr 14 19:01:08 +0000 2020",
+		"id": 1250137028315902000,
+		"id_str": "1250137028315901953",
+		"full_text": "@pm_animation @travismcelroy This is perfect. Thank you. I really do think my favorite part of the segment is how much @JustinMcElroy and Griffin are annoyed by it. Also, just how much work @travismcelroy puts into it. Seems like a long-con that I just can’t quite figure out yet.",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "pm_animation",
+					"name": "✨Paige✨",
+					"id": 511274190,
+					"id_str": "511274190",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "travismcelroy",
+					"name": "Travis McElroy, The Internet's Best Friend",
+					"id": 19844987,
+					"id_str": "19844987",
+					"indices": [
+						14,
+						28
+					]
+				},
+				{
+					"screen_name": "JustinMcElroy",
+					"name": "Justin “Hoops” McElroy",
+					"id": 16421419,
+					"id_str": "16421419",
+					"indices": [
+						119,
+						133
+					]
+				},
+				{
+					"screen_name": "travismcelroy",
+					"name": "Travis McElroy, The Internet's Best Friend",
+					"id": 19844987,
+					"id_str": "19844987",
+					"indices": [
+						190,
+						204
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1250077728134639600,
+		"in_reply_to_status_id_str": "1250077728134639617",
+		"in_reply_to_user_id": 511274190,
+		"in_reply_to_user_id_str": "511274190",
+		"in_reply_to_screen_name": "pm_animation",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Senior Operations Manager for Admissions @MinervaSchools. Proud husband; father of 3 strange and beautiful children. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6480,
+			"friends_count": 2685,
+			"listed_count": 373,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1371,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18431,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@pm_animation @travismcelroy This is perfect. Thank you. I really do think my favorite part of the segment is how much @JustinMcElroy and Griffin are annoyed by it. Also, just how much work @travismcelroy puts into it. Seems like a long-con that I just can’t quite figure out yet."
+	},
+	{
 		"created_at": "Tue Apr 14 17:03:08 +0000 2020",
 		"id": 1250107332547063800,
 		"id_str": "1250107332547063809",

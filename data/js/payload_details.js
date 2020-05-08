4ca2e2a43c0a244d86675ff0b0574d4e97c40857
bhwilkoff <benwilkoff@gmail.com>
2020-05-08 10:22:02 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18604,
-	"created_at": "Thu May 07 00:22:00 +0000 2020",
+	"tweets": 18605,
+	"created_at": "Fri May 08 16:22:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Fri May 08 15:24:56 +0000 2020",
+		"id": 1258779926859149300,
+		"id_str": "1258779926859149312",
+		"full_text": "@Madeline_Gobbo @clapifyoulikeme What’s the story, wishbone?",
+		"truncated": false,
+		"display_text_range": [
+			33,
+			60
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Madeline_Gobbo",
+					"name": "a spooky little meatball",
+					"id": 570550038,
+					"id_str": "570550038",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "clapifyoulikeme",
+					"name": "Detective Pikajew",
+					"id": 14381110,
+					"id_str": "14381110",
+					"indices": [
+						16,
+						32
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1258450698187882500,
+		"in_reply_to_status_id_str": "1258450698187882497",
+		"in_reply_to_user_id": 570550038,
+		"in_reply_to_user_id_str": "570550038",
+		"in_reply_to_screen_name": "Madeline_Gobbo",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Senior Operations Manager for Admissions @MinervaSchools. Proud husband; father of 3 strange and beautiful children. Also, I Teach. And Learn. A Lot.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6487,
+			"friends_count": 2851,
+			"listed_count": 371,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1513,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18460,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Madeline_Gobbo @clapifyoulikeme What’s the story, wishbone?"
+	},
+	{
 		"created_at": "Wed May 06 23:53:17 +0000 2020",
 		"id": 1258183082827341800,
 		"id_str": "1258183082827341829",

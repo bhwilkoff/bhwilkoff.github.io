a49d40de3f2b624bb598c51c1eede20b3dd34c03
bhwilkoff <benwilkoff@gmail.com>
2020-07-06 16:22:03 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18625,
-	"created_at": "Thu Jul 02 23:22:01 +0000 2020",
+	"tweets": 18626,
+	"created_at": "Mon Jul 06 22:22:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Mon Jul 06 22:01:10 +0000 2020",
+		"id": 1280260526841319400,
+		"id_str": "1280260526841319424",
+		"full_text": "@JoyKirr @paulbogush Thank you for the incredible walk down memory lane! Open to collaborating any time, but I can’t promise it will be anything like 2007.",
+		"truncated": false,
+		"display_text_range": [
+			21,
+			155
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JoyKirr",
+					"name": "Joy Kirr",
+					"id": 344350209,
+					"id_str": "344350209",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "paulbogush",
+					"name": "Paul Bogush",
+					"id": 6014812,
+					"id_str": "6014812",
+					"indices": [
+						9,
+						20
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1280212183322853400,
+		"in_reply_to_status_id_str": "1280212183322853385",
+		"in_reply_to_user_id": 344350209,
+		"in_reply_to_user_id_str": "344350209",
+		"in_reply_to_screen_name": "JoyKirr",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Senior Operations Manager for Admissions @MinervaSchools. Anti-Racist. Equity-focused. Growth-oriented Husband; father of 3 strange and beautiful children.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6450,
+			"friends_count": 3018,
+			"listed_count": 369,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1715,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18482,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@JoyKirr @paulbogush Thank you for the incredible walk down memory lane! Open to collaborating any time, but I can’t promise it will be anything like 2007."
+	},
+	{
 		"created_at": "Thu Jul 02 22:37:49 +0000 2020",
 		"id": 1278820197910999000,
 		"id_str": "1278820197910999041",

220882bc2ef5e877553b0faea6a4ea8edbce401f
bhwilkoff <benwilkoff@gmail.com>
2020-08-04 23:22:03 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18631,
-	"created_at": "Wed Jul 29 15:22:02 +0000 2020",
+	"tweets": 18632,
+	"created_at": "Wed Aug 05 05:22:02 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_08.js
@@ -0,0 +1,119 @@
+Grailbird.data.tweets_2020_08 = 
+[
+	{
+		"created_at": "Wed Aug 05 05:04:19 +0000 2020",
+		"id": 1290876266241876000,
+		"id_str": "1290876266241875969",
+		"full_text": "@AngieTheOptimus @DemocracyStorm That is perfect. Also, exactly where we are headed.",
+		"truncated": false,
+		"display_text_range": [
+			33,
+			84
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AngieTheOptimus",
+					"name": "Angie the Optimist",
+					"id": 808341074636841000,
+					"id_str": "808341074636840960",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "DemocracyStorm",
+					"name": "Lori Coleman #BlackLivesMatter",
+					"id": 843556423548067800,
+					"id_str": "843556423548067840",
+					"indices": [
+						17,
+						32
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1290875941590204400,
+		"in_reply_to_status_id_str": "1290875941590204417",
+		"in_reply_to_user_id": 808341074636841000,
+		"in_reply_to_user_id_str": "808341074636840960",
+		"in_reply_to_screen_name": "AngieTheOptimus",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Senior Operations Manager for Admissions @MinervaSchools. Anti-Racist. Equity-focused. Growth-oriented Husband; father of 3 strange and beautiful children.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6436,
+			"friends_count": 3057,
+			"listed_count": 367,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1786,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18483,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@AngieTheOptimus @DemocracyStorm That is perfect. Also, exactly where we are headed."
+	}
+]

105201069ca60c2da8c9f1c36c1c5723a57f0625
bhwilkoff <benwilkoff@gmail.com>
2020-10-20 09:22:01 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18640,
-	"created_at": "Wed Sep 16 22:22:02 +0000 2020",
+	"tweets": 18641,
+	"created_at": "Tue Oct 20 15:22:00 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_10.js
@@ -0,0 +1,149 @@
+Grailbird.data.tweets_2020_10 = 
+[
+	{
+		"created_at": "Tue Oct 20 14:29:50 +0000 2020",
+		"id": 1318560058666590200,
+		"id_str": "1318560058666590210",
+		"full_text": "I voted today. For @JoeBiden. For @KamalaHarris. For @Hickenlooper. For @JasonCrowCO6. For @DavidDOrtizCO. For Colorado. For the UNITED states. For my children. For justice. For honesty. For equality. For black lives. For progress. Not against anything. Only FOR the future.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JoeBiden",
+					"name": "Joe Biden",
+					"id": 939091,
+					"id_str": "939091",
+					"indices": [
+						19,
+						28
+					]
+				},
+				{
+					"screen_name": "KamalaHarris",
+					"name": "Kamala Harris",
+					"id": 30354991,
+					"id_str": "30354991",
+					"indices": [
+						34,
+						47
+					]
+				},
+				{
+					"screen_name": "Hickenlooper",
+					"name": "John Hickenlooper",
+					"id": 117839957,
+					"id_str": "117839957",
+					"indices": [
+						53,
+						66
+					]
+				},
+				{
+					"screen_name": "JasonCrowCO6",
+					"name": "Jason Crow",
+					"id": 851477335052140500,
+					"id_str": "851477335052140544",
+					"indices": [
+						72,
+						85
+					]
+				},
+				{
+					"screen_name": "DavidDOrtizCO",
+					"name": "David Ortiz",
+					"id": 1127043616805797900,
+					"id_str": "1127043616805797888",
+					"indices": [
+						91,
+						105
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Associate Director for Admissions Operations @MinervaSchools. Anti-Racist. Equity-focused. Growth-oriented Husband; father of 3 strange and beautiful children.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6428,
+			"friends_count": 3204,
+			"listed_count": 362,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 1955,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18489,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "I voted today. For @JoeBiden. For @KamalaHarris. For @Hickenlooper. For @JasonCrowCO6. For @DavidDOrtizCO. For Colorado. For the UNITED states. For my children. For justice. For honesty. For equality. For black lives. For progress. Not against anything. Only FOR the future."
+	}
+]

c74c81e0360a48557078fb1c0b63c71485d3159c
bhwilkoff <benwilkoff@gmail.com>
2020-12-10 10:22:01 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18649,
-	"created_at": "Tue Dec 08 17:22:01 +0000 2020",
+	"tweets": 18650,
+	"created_at": "Thu Dec 10 17:22:00 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,313 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Thu Dec 10 16:59:42 +0000 2020",
+		"id": 1337079555769561000,
+		"id_str": "1337079555769561090",
+		"full_text": "Have you seen these @audreywatters? https://t.co/8Wb3d8Uqfr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "audreywatters",
+					"name": "Audrey Watters",
+					"id": 25388528,
+					"id_str": "25388528",
+					"indices": [
+						20,
+						34
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8Wb3d8Uqfr",
+					"expanded_url": "https://twitter.com/_patmurray/status/1337074764788068353",
+					"display_url": "twitter.com/_patmurray/sta…",
+					"indices": [
+						36,
+						59
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Associate Director for Admissions Operations @MinervaSchools. Anti-Racist. Equity-focused. Growth-oriented Husband; father of 3 strange and beautiful children.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6398,
+			"friends_count": 3263,
+			"listed_count": 361,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 2128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18494,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1337074764788068400,
+		"quoted_status_id_str": "1337074764788068353",
+		"quoted_status_permalink": {
+			"url": "https://t.co/8Wb3d8Uqfr",
+			"expanded": "https://twitter.com/_patmurray/status/1337074764788068353",
+			"display": "twitter.com/_patmurray/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Dec 10 16:40:40 +0000 2020",
+			"id": 1337074764788068400,
+			"id_str": "1337074764788068353",
+			"full_text": "allBIRBs https://t.co/mtJOurRety https://t.co/GcSNxZozGk",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				32
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/mtJOurRety",
+						"expanded_url": "https://www.allbirds.com/products/mens-staple-dashers",
+						"display_url": "allbirds.com/products/mens-…",
+						"indices": [
+							9,
+							32
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1337074761323565000,
+						"id_str": "1337074761323565057",
+						"indices": [
+							33,
+							56
+						],
+						"media_url": "http://pbs.twimg.com/media/Eo4-aUrU0AEtDEN.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Eo4-aUrU0AEtDEN.jpg",
+						"url": "https://t.co/GcSNxZozGk",
+						"display_url": "pic.twitter.com/GcSNxZozGk",
+						"expanded_url": "https://twitter.com/_patmurray/status/1337074764788068353/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1124,
+								"h": 1522,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 886,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 502,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1337074761323565000,
+						"id_str": "1337074761323565057",
+						"indices": [
+							33,
+							56
+						],
+						"media_url": "http://pbs.twimg.com/media/Eo4-aUrU0AEtDEN.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Eo4-aUrU0AEtDEN.jpg",
+						"url": "https://t.co/GcSNxZozGk",
+						"display_url": "pic.twitter.com/GcSNxZozGk",
+						"expanded_url": "https://twitter.com/_patmurray/status/1337074764788068353/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1124,
+								"h": 1522,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 886,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 502,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 833064800,
+				"id_str": "833064800",
+				"name": "Pat Murray 🏳️‍🌈",
+				"screen_name": "_patmurray",
+				"location": "a sydney boy in cupertino",
+				"description": "🦩🦆🦔📷🎹🎬⛷🏍🚲✈️ — iCloud Photos at  — he/him 🏳️‍🌈",
+				"url": "https://t.co/56IGrjZl4Q",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/56IGrjZl4Q",
+								"expanded_url": "https://patmurray.co",
+								"display_url": "patmurray.co",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2846,
+				"friends_count": 360,
+				"listed_count": 116,
+				"created_at": "Wed Sep 19 10:52:12 +0000 2012",
+				"favourites_count": 98603,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 42458,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "7E8E9E",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1278768331399417856/5zsUMEFc_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278768331399417856/5zsUMEFc_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/833064800/1463214306",
+				"profile_link_color": "981CEB",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "09B8A6",
+				"profile_text_color": "088CA6",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 13,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Have you seen these @audreywatters? https://t.co/8Wb3d8Uqfr"
+	},
+	{
 		"created_at": "Tue Dec 08 16:55:47 +0000 2020",
 		"id": 1336353796146954200,
 		"id_str": "1336353796146954240",

9ffb956b4a12be8c98aaa26d0f4d28f3e9f173c7
bhwilkoff <benwilkoff@gmail.com>
2021-02-06 20:22:02 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18651,
-	"created_at": "Tue Jan 12 18:22:00 +0000 2021",
+	"tweets": 18652,
+	"created_at": "Sun Feb 07 03:22:00 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_02.js
@@ -0,0 +1,302 @@
+Grailbird.data.tweets_2021_02 = 
+[
+	{
+		"created_at": "Sun Feb 07 02:52:30 +0000 2021",
+		"id": 1358247236778426400,
+		"id_str": "1358247236778426371",
+		"full_text": "This is important. And, I need help. Black Lives clearly don’t matter to our HOA.\n\nPlease re-share so we can continue to fight for #socialjustice in our neighborhood. @ACLUofColorado @9news #BLM #BlackHistoryMonth \n\nShare advice you have. We need change.\nhttps://t.co/AGxzqhwien https://t.co/6RqV3bvFsb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "socialjustice",
+					"indices": [
+						131,
+						145
+					]
+				},
+				{
+					"text": "BLM",
+					"indices": [
+						190,
+						194
+					]
+				},
+				{
+					"text": "BlackHistoryMonth",
+					"indices": [
+						195,
+						213
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ACLUofColorado",
+					"name": "ACLU of Colorado",
+					"id": 155598923,
+					"id_str": "155598923",
+					"indices": [
+						167,
+						182
+					]
+				},
+				{
+					"screen_name": "9NEWS",
+					"name": "9NEWS Denver",
+					"id": 19032473,
+					"id_str": "19032473",
+					"indices": [
+						183,
+						189
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AGxzqhwien",
+					"expanded_url": "https://www.instagram.com/p/CK-WV5tlxQM/",
+					"display_url": "instagram.com/p/CK-WV5tlxQM/",
+					"indices": [
+						255,
+						278
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1358247229685780500,
+					"id_str": "1358247229685780480",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/Etl2qLqUUAA7fGJ.png",
+					"media_url_https": "https://pbs.twimg.com/media/Etl2qLqUUAA7fGJ.png",
+					"url": "https://t.co/6RqV3bvFsb",
+					"display_url": "pic.twitter.com/6RqV3bvFsb",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1358247236778426371/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 763,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 432,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1610,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1358247229685780500,
+					"id_str": "1358247229685780480",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/Etl2qLqUUAA7fGJ.png",
+					"media_url_https": "https://pbs.twimg.com/media/Etl2qLqUUAA7fGJ.png",
+					"url": "https://t.co/6RqV3bvFsb",
+					"display_url": "pic.twitter.com/6RqV3bvFsb",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1358247236778426371/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 763,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 432,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1610,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1358247232630255600,
+					"id_str": "1358247232630255618",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/Etl2qWoVcAISAb0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Etl2qWoVcAISAb0.jpg",
+					"url": "https://t.co/6RqV3bvFsb",
+					"display_url": "pic.twitter.com/6RqV3bvFsb",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1358247236778426371/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1358247234714833000,
+					"id_str": "1358247234714832897",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/Etl2qeZVkAEcyau.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Etl2qeZVkAEcyau.jpg",
+					"url": "https://t.co/6RqV3bvFsb",
+					"display_url": "pic.twitter.com/6RqV3bvFsb",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1358247236778426371/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 885,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 502,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1388,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://pragmaticcode.com/linky/\" rel=\"nofollow\">Linky for iOS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Associate Director for Admissions Operations @MinervaSchools. Anti-Racist. Equity-focused. Growth-oriented Husband; father of 3 strange and beautiful children.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6345,
+			"friends_count": 3306,
+			"listed_count": 352,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 2237,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18494,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is important. And, I need help. Black Lives clearly don’t matter to our HOA.\n\nPlease re-share so we can continue to fight for #socialjustice in our neighborhood. @ACLUofColorado @9news #BLM #BlackHistoryMonth \n\nShare advice you have. We need change.\nhttps://t.co/AGxzqhwien https://t.co/6RqV3bvFsb"
+	}
+]

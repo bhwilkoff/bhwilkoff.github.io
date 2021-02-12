f9f822ced8806196c392510cdb10b0d7caeeef72
bhwilkoff <benwilkoff@gmail.com>
2021-02-12 08:22:02 -0700

    Added 1 tweets

--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,254 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Fri Feb 12 15:18:44 +0000 2021",
+		"id": 1360078148986142700,
+		"id_str": "1360246968694362113",
+		"full_text": "A couple was told they can’t fly their Black Lives Matter flag.\n\nA neighbor asked the HOA to hold an emergency meeting. \n\nHe said: “The concerns about property values is even greater now with bad media publicity.” \n\nBad media publicity: https://t.co/VmW6mk6tbU https://t.co/7YplmJRsR5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			260
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VmW6mk6tbU",
+					"expanded_url": "https://cbsloc.al/2MWj6tr",
+					"display_url": "cbsloc.al/2MWj6tr",
+					"indices": [
+						237,
+						260
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1360077968886882300,
+					"id_str": "1360077968886882309",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1360077968886882309/pu/img/ARJgNQNTgQ_H2IB2.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1360077968886882309/pu/img/ARJgNQNTgQ_H2IB2.jpg",
+					"url": "https://t.co/7YplmJRsR5",
+					"display_url": "pic.twitter.com/7YplmJRsR5",
+					"expanded_url": "https://twitter.com/ToriMasonTV/status/1360078148986142723/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1272,
+							"h": 720,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 679,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 385,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1360077968886882300,
+					"id_str": "1360077968886882309",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1360077968886882309/pu/img/ARJgNQNTgQ_H2IB2.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1360077968886882309/pu/img/ARJgNQNTgQ_H2IB2.jpg",
+					"url": "https://t.co/7YplmJRsR5",
+					"display_url": "pic.twitter.com/7YplmJRsR5",
+					"expanded_url": "https://twitter.com/ToriMasonTV/status/1360078148986142723/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 1272,
+							"h": 720,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 679,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 385,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							53,
+							30
+						],
+						"duration_millis": 133382,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1360077968886882309/pu/pl/OaTi1oMNXPSqxURs.m3u8?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1360077968886882309/pu/vid/476x270/UamiC4JQLfzCjkHa.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1360077968886882309/pu/vid/636x360/y9Tu8ebMyK9Tt-q6.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1360077968886882309/pu/vid/1272x720/i7vbk2HZCpRltJy5.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3313889323,
+			"id_str": "3313889323",
+			"name": "Tori Mason",
+			"screen_name": "ToriMasonTV",
+			"location": "Orlando → Denver",
+			"description": "Emmy-winning Journalist @CBSDenver CBS4 • LSU @ManshipSchool • NABJ • Florida Woman☀️ #GeauxTigers",
+			"url": "https://t.co/QrxCdyloly",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QrxCdyloly",
+							"expanded_url": "http://CBSdenver.com",
+							"display_url": "CBSdenver.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4359,
+			"friends_count": 791,
+			"listed_count": 59,
+			"created_at": "Thu Aug 13 00:51:23 +0000 2015",
+			"favourites_count": 8053,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13560,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1359390543084666880/9SjiotTk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1359390543084666880/9SjiotTk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3313889323/1594623416",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "b49b3053b5c25bf5",
+			"url": "https://api.twitter.com/1.1/geo/id/b49b3053b5c25bf5.json",
+			"place_type": "city",
+			"name": "Denver",
+			"full_name": "Denver, CO",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-105.109815,
+							39.614151
+						],
+						[
+							-104.734372,
+							39.614151
+						],
+						[
+							-104.734372,
+							39.812975
+						],
+						[
+							-105.109815,
+							39.812975
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 68,
+		"favorite_count": 251,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A couple was told they can’t fly their Black Lives Matter flag.\n\nA neighbor asked the HOA to hold an emergency meeting. \n\nHe said: “The concerns about property values is even greater now with bad media publicity.” \n\nBad media publicity: https://t.co/VmW6mk6tbU https://t.co/7YplmJRsR5"
+	},
+	{
 		"created_at": "Fri Feb 12 04:20:30 +0000 2021",
 		"id": 1360081318856466400,
 		"id_str": "1360081318856466433",

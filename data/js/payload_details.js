cd607d951ed49bde690906915c1a9d9a48ddf1ee
bhwilkoff <benwilkoff@gmail.com>
2021-03-03 11:22:00 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18660,
-	"created_at": "Wed Feb 24 16:22:00 +0000 2021",
+	"tweets": 18661,
+	"created_at": "Wed Mar 03 18:22:00 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_03.js
@@ -0,0 +1,223 @@
+Grailbird.data.tweets_2021_03 = 
+[
+	{
+		"created_at": "Wed Mar 03 17:45:48 +0000 2021",
+		"id": 1367169351439163400,
+		"id_str": "1367169351439163393",
+		"full_text": "Wealth begets wealth. \n\nI have yet to see good resources for teaching about generational wealth in k-12. \n\nWhy is this so hard to understand? \n\nFor Black Americans, the middle class has always been a mirage https://t.co/zfRT8KqLre https://t.co/MOT940o3p4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			230
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zfRT8KqLre",
+					"expanded_url": "https://www.vox.com/the-goods/22245223/black-middle-class-racism-reparations",
+					"display_url": "vox.com/the-goods/2224…",
+					"indices": [
+						207,
+						230
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1367169345923649500,
+					"id_str": "1367169345923649540",
+					"indices": [
+						231,
+						254
+					],
+					"media_url": "http://pbs.twimg.com/media/EvkpRsuVEAQkO7A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EvkpRsuVEAQkO7A.jpg",
+					"url": "https://t.co/MOT940o3p4",
+					"display_url": "pic.twitter.com/MOT940o3p4",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1367169351439163393/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1057,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 659,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1057,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1367169345923649500,
+					"id_str": "1367169345923649540",
+					"indices": [
+						231,
+						254
+					],
+					"media_url": "http://pbs.twimg.com/media/EvkpRsuVEAQkO7A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EvkpRsuVEAQkO7A.jpg",
+					"url": "https://t.co/MOT940o3p4",
+					"display_url": "pic.twitter.com/MOT940o3p4",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1367169351439163393/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1057,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 659,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1057,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1367169348809330700,
+					"id_str": "1367169348809330690",
+					"indices": [
+						231,
+						254
+					],
+					"media_url": "http://pbs.twimg.com/media/EvkpR3eVEAIXmM4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EvkpR3eVEAIXmM4.jpg",
+					"url": "https://t.co/MOT940o3p4",
+					"display_url": "pic.twitter.com/MOT940o3p4",
+					"expanded_url": "https://twitter.com/bhwilkoff/status/1367169351439163393/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://pragmaticcode.com/linky/\" rel=\"nofollow\">Linky for iOS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Associate Director for Admissions Operations @MinervaSchools. Anti-Racist. Equity-focused. Growth-oriented Husband; father of 3 strange and beautiful children.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6343,
+			"friends_count": 3332,
+			"listed_count": 353,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 2259,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18501,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Wealth begets wealth. \n\nI have yet to see good resources for teaching about generational wealth in k-12. \n\nWhy is this so hard to understand? \n\nFor Black Americans, the middle class has always been a mirage https://t.co/zfRT8KqLre https://t.co/MOT940o3p4"
+	}
+]

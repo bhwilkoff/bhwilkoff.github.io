c5c36b5b29d16275578e9f07099f17952b5d4f0c
bhwilkoff <benwilkoff@gmail.com>
2021-05-07 16:22:01 -0600

    Added 1 tweets

--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,328 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Fri May 07 22:11:04 +0000 2021",
+		"id": 1390791318591348700,
+		"id_str": "1390791318591348736",
+		"full_text": "My daughter sometimes makes MS Paint doodles. Which, apparently, are still a thing. https://t.co/ZK8g5NOXSL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			83
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ZK8g5NOXSL",
+					"expanded_url": "https://twitter.com/ac1nnabun/status/1390777269967233026",
+					"display_url": "twitter.com/ac1nnabun/stat…",
+					"indices": [
+						84,
+						107
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Associate Director for Admissions Operations @MinervaSchools. Anti-Racist. Equity-focused. Growth-oriented Husband; father of 3 strange and beautiful children.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6339,
+			"friends_count": 3381,
+			"listed_count": 351,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 2365,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18518,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1390777269967233000,
+		"quoted_status_id_str": "1390777269967233026",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ZK8g5NOXSL",
+			"expanded": "https://twitter.com/ac1nnabun/status/1390777269967233026",
+			"display": "twitter.com/ac1nnabun/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Fri May 07 21:15:15 +0000 2021",
+			"id": 1390777269967233000,
+			"id_str": "1390777269967233026",
+			"full_text": "Somewhere from my mind this image was born, thanks for the inspo @Coldoggo \n\n...cheese https://t.co/S9CDjdw2m9",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				86
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Coldoggo",
+						"name": "toast ⚢🍞 @FINALS | 1312 BL🅼 Stop Asian Hate",
+						"id": 865694554414092300,
+						"id_str": "865694554414092288",
+						"indices": [
+							65,
+							74
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1390776802038009900,
+						"id_str": "1390776802038009856",
+						"indices": [
+							87,
+							110
+						],
+						"media_url": "http://pbs.twimg.com/media/E00IIf9UUAAPQNU.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E00IIf9UUAAPQNU.jpg",
+						"url": "https://t.co/S9CDjdw2m9",
+						"display_url": "pic.twitter.com/S9CDjdw2m9",
+						"expanded_url": "https://twitter.com/ac1nnabun/status/1390777269967233026/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 435,
+								"h": 442,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 435,
+								"h": 442,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 435,
+								"h": 442,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1390776802038009900,
+						"id_str": "1390776802038009856",
+						"indices": [
+							87,
+							110
+						],
+						"media_url": "http://pbs.twimg.com/media/E00IIf9UUAAPQNU.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E00IIf9UUAAPQNU.jpg",
+						"url": "https://t.co/S9CDjdw2m9",
+						"display_url": "pic.twitter.com/S9CDjdw2m9",
+						"expanded_url": "https://twitter.com/ac1nnabun/status/1390777269967233026/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 435,
+								"h": 442,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 435,
+								"h": 442,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 435,
+								"h": 442,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1390777037447520300,
+						"id_str": "1390777037447520259",
+						"indices": [
+							87,
+							110
+						],
+						"media_url": "http://pbs.twimg.com/media/E00IWM7UYAM5Ev8.png",
+						"media_url_https": "https://pbs.twimg.com/media/E00IWM7UYAM5Ev8.png",
+						"url": "https://t.co/S9CDjdw2m9",
+						"display_url": "pic.twitter.com/S9CDjdw2m9",
+						"expanded_url": "https://twitter.com/ac1nnabun/status/1390777269967233026/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 372,
+								"h": 267,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 372,
+								"h": 267,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 372,
+								"h": 267,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1380286073448714200,
+				"id_str": "1380286073448714244",
+				"name": "Cinnabun",
+				"screen_name": "ac1nnabun",
+				"location": "Open for requests! ",
+				"description": "Cinna/Cin/Bun | Any pronouns work! | COMISSIONS OPEN SOON | ENFP-T | Will get back to you within a day! | Retweets are really appreciated but no pressure!",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 7,
+				"friends_count": 62,
+				"listed_count": 0,
+				"created_at": "Thu Apr 08 22:27:10 +0000 2021",
+				"favourites_count": 25,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 16,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1380286474487164930/tsW-TrGd_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1380286474487164930/tsW-TrGd_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1380286073448714244/1618022662",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 31,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My daughter sometimes makes MS Paint doodles. Which, apparently, are still a thing. https://t.co/ZK8g5NOXSL"
+	},
+	{
 		"created_at": "Fri May 07 20:08:41 +0000 2021",
 		"id": 1390760520836210700,
 		"id_str": "1390760520836210688",

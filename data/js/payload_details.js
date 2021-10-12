34f2df64ee1c78de0a130e88eb85b0eeef4cd032
bhwilkoff <benwilkoff@gmail.com>
2021-10-12 14:22:01 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18693,
-	"created_at": "Mon Oct 11 16:22:00 +0000 2021",
+	"tweets": 18694,
+	"created_at": "Tue Oct 12 20:22:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Tue Oct 12 20:21:39 +0000 2021",
+		"id": 1448021066295562200,
+		"id_str": "1448021066295562243",
+		"full_text": "What at invaluable resource for anyone who takes screenshots. So great! https://t.co/W1acGnbEDR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/W1acGnbEDR",
+					"expanded_url": "https://twitter.com/macstoriesnet/status/1447932646697406466",
+					"display_url": "twitter.com/macstoriesnet/…",
+					"indices": [
+						72,
+						95
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Associate Director for Admissions Operations @MinervaSchools. Anti-Racist. Equity-focused. Growth-oriented Husband; father of 3 strange and beautiful children.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6277,
+			"friends_count": 3379,
+			"listed_count": 350,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 2377,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18531,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1447932646697406500,
+		"quoted_status_id_str": "1447932646697406466",
+		"quoted_status_permalink": {
+			"url": "https://t.co/W1acGnbEDR",
+			"expanded": "https://twitter.com/macstoriesnet/status/1447932646697406466",
+			"display": "twitter.com/macstoriesnet/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Oct 12 14:30:18 +0000 2021",
+			"id": 1447932646697406500,
+			"id_str": "1447932646697406466",
+			"full_text": "Apple Frames 2.0: Faster, Lighter, and Featuring Support for iPhone 13, iPad mini, iPad 10.2″, iMac 24″, MacBook Air, and Multiple Languages https://t.co/KhZYU7FsAB",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				164
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/KhZYU7FsAB",
+						"expanded_url": "https://www.macstories.net/ios/apple-frames-2-0-faster-lighter-and-featuring-support-for-iphone-13-ipad-mini-ipad-10-2-imac-24-macbook-air-and-multiple-languages/",
+						"display_url": "macstories.net/ios/apple-fram…",
+						"indices": [
+							141,
+							164
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">WordPress.com</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 87077721,
+				"id_str": "87077721",
+				"name": "MacStories",
+				"screen_name": "macstoriesnet",
+				"location": "",
+				"description": "Apple news, app reviews, and stories. By @viticci and friends.  Advertise: https://t.co/bJlL8NAotW  Want more? Join @ClubMacStories: https://t.co/FMY97egcPc",
+				"url": "https://t.co/rz6ZEw9ok7",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/rz6ZEw9ok7",
+								"expanded_url": "http://www.macstories.net/",
+								"display_url": "macstories.net",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/bJlL8NAotW",
+								"expanded_url": "http://macstories.net/advertise",
+								"display_url": "macstories.net/advertise",
+								"indices": [
+									75,
+									98
+								]
+							},
+							{
+								"url": "https://t.co/FMY97egcPc",
+								"expanded_url": "http://club.macstories.net",
+								"display_url": "club.macstories.net",
+								"indices": [
+									133,
+									156
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 49629,
+				"friends_count": 8,
+				"listed_count": 3181,
+				"created_at": "Tue Nov 03 01:16:41 +0000 2009",
+				"favourites_count": 18,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 19084,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EEEEEE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/692115446456258560/HIjZquav_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/692115446456258560/HIjZquav_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/87077721/1398257369",
+				"profile_link_color": "545859",
+				"profile_sidebar_border_color": "5B5C5C",
+				"profile_sidebar_fill_color": "CDD0D1",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 27,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "What at invaluable resource for anyone who takes screenshots. So great! https://t.co/W1acGnbEDR"
+	},
+	{
 		"created_at": "Mon Oct 11 15:24:04 +0000 2021",
 		"id": 1447583789039882200,
 		"id_str": "1447583789039882245",

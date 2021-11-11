7e32d56bf04943fdc307fd4a5a8fed46bf580869
bhwilkoff <benwilkoff@gmail.com>
2021-11-10 17:22:01 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18710,
-	"created_at": "Tue Nov 09 14:22:00 +0000 2021",
+	"tweets": 18711,
+	"created_at": "Thu Nov 11 00:22:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Wed Nov 10 23:27:59 +0000 2021",
+		"id": 1458577204426600400,
+		"id_str": "1458577204426600450",
+		"full_text": "@wilkoffe @aahales Best decision you ever made? Seems suspect.",
+		"truncated": false,
+		"display_text_range": [
+			19,
+			62
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wilkoffe",
+					"name": "Ephram (turkey stan) Wilkoff",
+					"id": 21721173,
+					"id_str": "21721173",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "aahales",
+					"name": "Ashley Hales",
+					"id": 19398497,
+					"id_str": "19398497",
+					"indices": [
+						10,
+						18
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1458552815928942600,
+		"in_reply_to_status_id_str": "1458552815928942593",
+		"in_reply_to_user_id": 21721173,
+		"in_reply_to_user_id_str": "21721173",
+		"in_reply_to_screen_name": "wilkoffe",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Associate Director for Admissions Operations @MinervaSchools. Anti-Racist. Equity-focused. Growth-oriented Husband; father of 3 strange and beautiful children.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6281,
+			"friends_count": 3413,
+			"listed_count": 350,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 2469,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18550,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@wilkoffe @aahales Best decision you ever made? Seems suspect."
+	},
+	{
 		"created_at": "Tue Nov 09 13:42:07 +0000 2021",
 		"id": 1458067379586404400,
 		"id_str": "1458067379586404352",

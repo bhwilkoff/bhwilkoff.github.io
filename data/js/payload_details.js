3d7c17d49b857a29af9415ff6d2fd13ca24e01c2
bhwilkoff <benwilkoff@gmail.com>
2021-12-01 13:22:02 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18714,
-	"created_at": "Wed Dec 01 00:22:00 +0000 2021",
+	"tweets": 18715,
+	"created_at": "Wed Dec 01 20:22:00 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_12.js
@@ -0,0 +1,99 @@
+Grailbird.data.tweets_2021_12 = 
+[
+	{
+		"created_at": "Wed Dec 01 20:11:55 +0000 2021",
+		"id": 1466116544228954000,
+		"id_str": "1466138010668339200",
+		"full_text": "I don’t just want a world where abortion care is legal.\n\nI want a world where I’m not harassed when I got my abortion. Where I’m not shamed. Where I’m not considered lucky to be able to get one.\n\nUpholding Roe v. Wade is the floor. We need abortion &amp; reproductive health justice.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4696552878,
+			"id_str": "4696552878",
+			"name": "Cori Bush",
+			"screen_name": "CoriBush",
+			"location": "St Louis, MO",
+			"description": "U.S. Congresswoman, MO-01. Nurse, activist, organizer, single mom, & pastor. Leading with love, fighting for regular people. #StLouisStrong (she/her)",
+			"url": "https://t.co/g5ToItg1eT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/g5ToItg1eT",
+							"expanded_url": "http://www.coribush.org/",
+							"display_url": "coribush.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 887330,
+			"friends_count": 2162,
+			"listed_count": 3005,
+			"created_at": "Sat Jan 02 14:36:12 +0000 2016",
+			"favourites_count": 14588,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8177,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1275804043785334784/7j5yj4ma_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1275804043785334784/7j5yj4ma_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4696552878/1604607334",
+			"profile_link_color": "2E529E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1423,
+		"favorite_count": 8785,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "I don’t just want a world where abortion care is legal.\n\nI want a world where I’m not harassed when I got my abortion. Where I’m not shamed. Where I’m not considered lucky to be able to get one.\n\nUpholding Roe v. Wade is the floor. We need abortion &amp; reproductive health justice."
+	}
+]

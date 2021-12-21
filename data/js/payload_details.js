746d0ce390b3b5a2ec91c35ee852e000ffb5ddea
bhwilkoff <benwilkoff@gmail.com>
2021-12-21 09:22:01 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18751,
-	"created_at": "Mon Dec 20 22:22:00 +0000 2021",
+	"tweets": 18752,
+	"created_at": "Tue Dec 21 16:22:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Tue Dec 21 15:37:14 +0000 2021",
+		"id": 1473316639244370000,
+		"id_str": "1473316639244369922",
+		"full_text": "It isn’t all about the economy for me, but I did also see a headline comparing Biden’s economic performance to Carter’s. This (more accurate) narrative of competent policies that lead to better outcomes for more people is the only one I’m interested in. https://t.co/eQS40QO3wm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			253
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/eQS40QO3wm",
+					"expanded_url": "https://twitter.com/whcos/status/1473314132921303046",
+					"display_url": "twitter.com/whcos/status/1…",
+					"indices": [
+						254,
+						277
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Associate Director for Admissions Operations @MinervaSchools. Anti-Racist. Equity-focused. Growth-oriented Husband; father of 3 strange and beautiful children.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6274,
+			"friends_count": 3467,
+			"listed_count": 346,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 2600,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18599,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1473314132921303000,
+		"quoted_status_id_str": "1473314132921303046",
+		"quoted_status_permalink": {
+			"url": "https://t.co/eQS40QO3wm",
+			"expanded": "https://twitter.com/whcos/status/1473314132921303046",
+			"display": "twitter.com/whcos/status/1…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Dec 21 15:27:16 +0000 2021",
+			"id": 1473314132921303000,
+			"id_str": "1473314132921303046",
+			"full_text": "\"U.S. financial markets are outperforming the world by the biggest margin in the 21st century, and with good reason: America’s economy improved more in Joe Biden's first 12 months than any president during the past 50 years...\" \n\nhttps://t.co/LSbBJpUUOo",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				253
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/LSbBJpUUOo",
+						"expanded_url": "https://www.bloomberg.com/opinion/articles/2021-12-20/president-biden-s-economic-performance-has-proved-unbeatable",
+						"display_url": "bloomberg.com/opinion/articl…",
+						"indices": [
+							230,
+							253
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1351284403801108500,
+				"id_str": "1351284403801108481",
+				"name": "Ronald Klain",
+				"screen_name": "WHCOS",
+				"location": "",
+				"description": "White House Chief of Staff / Proud Hoosier / #Team46",
+				"url": "https://t.co/NgUd6eRpNj",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/NgUd6eRpNj",
+								"expanded_url": "http://whitehouse.gov",
+								"display_url": "whitehouse.gov",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 533940,
+				"friends_count": 551,
+				"listed_count": 2569,
+				"created_at": "Mon Jan 18 21:47:51 +0000 2021",
+				"favourites_count": 9653,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 9115,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1351402577183862786/-fck7E6h_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1351402577183862786/-fck7E6h_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1351284403801108481/1611034563",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 350,
+			"favorite_count": 1130,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It isn’t all about the economy for me, but I did also see a headline comparing Biden’s economic performance to Carter’s. This (more accurate) narrative of competent policies that lead to better outcomes for more people is the only one I’m interested in. https://t.co/eQS40QO3wm"
+	},
+	{
 		"created_at": "Mon Dec 20 21:37:39 +0000 2021",
 		"id": 1473044952401662000,
 		"id_str": "1473044952401661953",

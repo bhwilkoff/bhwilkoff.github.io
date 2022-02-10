94f51e37d29d4b23bd4fa538bddc634d4251803b
bhwilkoff <benwilkoff@gmail.com>
2022-02-10 12:22:01 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18849,
-	"created_at": "Thu Feb 10 16:22:00 +0000 2022",
+	"tweets": 18850,
+	"created_at": "Thu Feb 10 19:22:00 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Thu Feb 10 19:02:38 +0000 2022",
+		"id": 1491850113135693800,
+		"id_str": "1491850113135693824",
+		"full_text": "@Collegewise @jennthetutor @CompassEduGroup Test free is the way to be!!!",
+		"truncated": false,
+		"display_text_range": [
+			44,
+			73
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Collegewise",
+					"name": "Collegewise",
+					"id": 130966284,
+					"id_str": "130966284",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "jennthetutor",
+					"name": "jennthetutor 🎓 👩🏾‍🏫 #TestFree (she/her)",
+					"id": 1140070669591502800,
+					"id_str": "1140070669591502848",
+					"indices": [
+						13,
+						26
+					]
+				},
+				{
+					"screen_name": "CompassEduGroup",
+					"name": "Compass Education Group",
+					"id": 3145713789,
+					"id_str": "3145713789",
+					"indices": [
+						27,
+						43
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1491837416272969700,
+		"in_reply_to_status_id_str": "1491837416272969733",
+		"in_reply_to_user_id": 130966284,
+		"in_reply_to_user_id_str": "130966284",
+		"in_reply_to_screen_name": "Collegewise",
+		"user": {
+			"id": 1710871,
+			"id_str": "1710871",
+			"name": "Ben Wilkoff",
+			"screen_name": "bhwilkoff",
+			"location": "Littleton, Co",
+			"description": "Associate Director for Admissions Operations @MinervaSchools. Anti-Racist. Equity-focused. Growth-oriented Husband; father of 3 strange and beautiful children.",
+			"url": "http://t.co/NW4gT6QJUF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NW4gT6QJUF",
+							"expanded_url": "http://learningischange.com",
+							"display_url": "learningischange.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6284,
+			"friends_count": 3555,
+			"listed_count": 346,
+			"created_at": "Wed Mar 21 04:14:02 +0000 2007",
+			"favourites_count": 2810,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18717,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136027222936936450/Bb_F1585_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710871/1563450278",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Collegewise @jennthetutor @CompassEduGroup Test free is the way to be!!!"
+	},
+	{
 		"created_at": "Thu Feb 10 16:08:44 +0000 2022",
 		"id": 1491806349956501500,
 		"id_str": "1491806349956501516",
